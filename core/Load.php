4c7e31246f98d7e24fae6ab6d1e9e01cc7f83786
Alex Keda <admin@lissyara.su>
2012-04-23 22:22:50 +0400

    JBS-443: restore version

--- core/Load.php
+++ core/Load.php
@@ -41,7 +41,7 @@ Ignore_User_Abort(TRUE); # Если пользователь закрыл сое
  * Defines system constants.
  */
 
-if(!Define('VERSION', 'v2.5.9'))
+if(!Define('VERSION', '##VERSION##'))
   Exit('[JBs core]: не удалось определить константу (VERSION)');
 
 /**
