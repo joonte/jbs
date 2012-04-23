36ab103a2fc07aa3d478074865ce4af7721b1e1d
Alex Keda <admin@lissyara.su>
2012-04-23 22:20:57 +0400

    JBS-443: fix request log

--- core/Load.php
+++ core/Load.php
@@ -41,7 +41,7 @@ Ignore_User_Abort(TRUE); # Если пользователь закрыл сое
  * Defines system constants.
  */
 
-if(!Define('VERSION', '##VERSION##'))
+if(!Define('VERSION', 'v2.5.9'))
   Exit('[JBs core]: не удалось определить константу (VERSION)');
 
 /**
@@ -480,11 +480,11 @@ function __ShutDown_Function__(){
     'COUNTER_COMPS'	=> $GLOBALS['__COUNTER_COMPS']
   );
   #---------------------------------------------------------------
-/*  if($GLOBALS['_SERVER']['REQUEST_URI'] != '/API/Events'){
+  if($GLOBALS['_SERVER']['REQUEST_URI'] != '/API/Events'){
     $IsInsert = DB_Insert('RequestLog',$UserData);
     if(Is_Error($IsInsert))
       return ERROR | @Trigger_Error(500);
-  }*/
+  }
   #-----------------------------------------------------------------------------
   #-----------------------------------------------------------------------------
   List($Micro,$Seconds) = Explode(' ',MicroTime());
