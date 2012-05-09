4e96d2f3c6c3ee49ad7a79c4b8b48f6b280f8dfe
Alex Keda <admin@lissyara.su>
2012-05-09 08:13:18 +0400

    JBS-336: fix chrate parameter

--- hosts/hosting/system/libs/VdsManager.php
+++ hosts/hosting/system/libs/VdsManager.php
@@ -62,7 +62,7 @@ function VdsManager_Create($Settings,$Login,$Password,$Domain,$IP,$VPSScheme,$Em
     'bmem'            => ceil($VPSScheme['bmem']),	# Burstable RAM
     ($IsReselling?'maxswap':'swap') => ceil($VPSScheme['maxswap']), # использование swap
     'traf'            => $VPSScheme['traf'],		# Трафик
-    'chrate'          => $VPSScheme['chrate'] * 1024 * 1024 ,   # канал, полоса (VDSmanager жрёт в битах, в биллинге - мегабиты)
+    'chrate'          => SPrintF('%umbit',$VPSScheme['chrate']),   # канал, полоса, мегабит
     'desc'            => $VPSScheme['maxdesc'],		# файловых дескрипторов
     'proc'            => $VPSScheme['proc'],		# процессов
     'ipcount'         => $VPSScheme['ipalias'],		# дополнительных IP
@@ -263,7 +263,7 @@ function VdsManager_Scheme_Change($Settings,$Login,$VPSScheme){
     'bmem'            => ceil($VPSScheme['bmem']),	# Burstable RAM
     ($IsReselling?'maxswap':'swap') => ceil($VPSScheme['maxswap']), # использование swap
     'traf'            => $VPSScheme['traf'],		# Трафик
-    'chrate'          => $VPSScheme['chrate'] * 1024 * 1024,   # канал, полоса (VDSmanager жрёт в килобитах, в биллинге - мегабиты)
+    'chrate'          => SPrintF('%umbit',$VPSScheme['chrate']),   # канал, полоса, мегабит
     'desc'            => $VPSScheme['maxdesc'],		# файловых дескрипторов
     'proc'            => $VPSScheme['proc'],		# процессов
     'ipcount'         => $VPSScheme['ipalias'],		# дополнительных IP
