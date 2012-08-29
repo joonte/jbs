653aad040cc30808ff6270b52745cabfe702346e
lissyara <admin@lissyara.su>
2012-08-29 12:10:08 +0400

    JBS-474: add timeout between HostingSuspend/HostingDelete tasks

--- hosts/hosting/comp/Triggers/Statuses/HostingOrders/OnCreate.comp.php
+++ hosts/hosting/comp/Triggers/Statuses/HostingOrders/OnCreate.comp.php
@@ -1,6 +1,5 @@
 <?php
 
-
 #-------------------------------------------------------------------------------
 /** @author Великодный В.В. (Joonte Ltd.) */
 /******************************************************************************/
@@ -10,7 +9,34 @@ $__args_list = Array('HostingOrder');
 Eval(COMP_INIT);
 /******************************************************************************/
 /******************************************************************************/
-$IsAdd = Comp_Load('www/Administrator/API/TaskEdit',Array('UserID'=>$HostingOrder['UserID'],'TypeID'=>'HostingCreate','Params'=>Array($HostingOrder['ID'])));
+#-------------------------------------------------------------------------------
+/*
+# added by lissyara 2012-08-29 in 10:20 MSK, for JBS-474
+$Where = Array("`TypeID` = 'HostingDelete' OR `TypeID` = 'HostingSuspend' OR `TypeID` = 'HostingActive' OR `TypeID` = 'HostingCreate'","`IsExecuted` = 'no'");
+#-------------------------------------------------------------------------------
+$TaskExecuteTime = DB_Select('Tasks','ExecuteDate',Array('UNIQ','Where'=>$Where,'SortOn'=>'ExecuteDate','IsDesc'=>TRUE,'Limits'=>Array(0,1)));
+#-------------------------------------------------------------------------------
+switch(ValueOf($TaskExecuteTime)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	$ExecuteDate = Time();
+	break;
+case 'array':
+	if($TaskExecuteTime['ExecuteDate'] > Time() + 2*3600){
+		$ExecuteDate = Time();
+	}else{
+		$ExecuteDate = $TaskExecuteTime['ExecuteDate'] + 2*60;
+	}
+	break;
+default:
+	return ERROR | @Trigger_Error(101);
+}
+*/
+$ExecuteDate = Time();
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$IsAdd = Comp_Load('www/Administrator/API/TaskEdit',Array('UserID'=>$HostingOrder['UserID'],'TypeID'=>'HostingCreate','ExecuteDate'=>$ExecuteDate,'Params'=>Array($HostingOrder['ID'])));
 #-------------------------------------------------------------------------------
 switch(ValueOf($IsAdd)){
   case 'error':
