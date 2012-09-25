9f242708b84f932cb1b4f1efef63a3382f0847f4
lissyara <admin@lissyara.su>
2012-09-25 14:38:56 +0400

    JBS-409: add tariffs for all

--- db/billing/db.sql
+++ db/billing/db.sql
@@ -786,8 +786,6 @@ set @Text = '<NOBODY>
    <TD align="justify" width="95%">Во всех случаях, не оговоренных и не предусмотренных в настоящем Договоре и/или Приложениях к настоящему Договору, стороны должны руководствоваться действующим законодательством Российской Федерации.</TD>
   </TR>
  </TABLE>
- <BR />
- <COMP path="Clauses/TariffList" args="NotUsed"> [список тарифов] </COMP>
 </NOBODY>';
 
 INSERT INTO `Clauses`
@@ -2772,6 +2770,8 @@ VALUES
 /* added by lissyara 2012-03-03 in 20:10 MSK, JBS-359 */
 INSERT INTO `Clauses` (`ID`, `PublicDate`, `ChangedDate`, `AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `Text`, `IsPublish`) VALUES
 ('', 1307536932, 0, 100, 100, 'Invoices/PaymentSystems/EasyPay', 'Шаблон платежной системы EasyPay', 'no', 'yes', 'yes', '<NOBODY>\r\n <H1>\r\n СЧЕТ №%Invoice.Number% от %Invoice.CreateDate%\r\n</H1>\r\n <DIV id="Services">\r\n [список услуг]\r\n</DIV>\r\n <H2>\r\n Платежное поручение\r\n</H2>\r\n <TABLE border="1" cellpadding="5" cellspacing="0">\r\n  <TBODY>\r\n   <TR bgcolor="#DCDCDC">\r\n    <TD align="center">\r\n    Назначение\r\n   </TD>\r\n    <TD align="center">\r\n    Номер Поставщика\r\n   </TD>\r\n    <TD align="center">\r\n    Сумма\r\n   </TD>\r\n   </TR>\r\n   <TR>\r\n    <TD>\r\n    За web-услуги по счету №%Invoice.Number%\r\n   </TD>\r\n    <TD align="right">\r\n    %PaymentSystem.Send.EP_MerNo%\r\n   </TD>\r\n    <TD align="right">\r\n    %Invoice.Foreign% %PaymentSystem.Measure%\r\n   </TD>\r\n   </TR>\r\n  </TBODY>\r\n </TABLE>\r\n</NOBODY>\r\n', 'yes');
+/* added by lissyara, 2012-09-25 in 14:37 MSK */
+INSERT INTO `Clauses` (`AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `IsPublish`, `Text`) VALUES (100, 100, 'Contracts/Enclosures/Types/Tariffs/Content', 'Тарифы', 'no', 'yes', 'yes', 'yes', '<NOBODY>\n <COMP path="Clauses/TariffList" args="NotUsed">\n [список тарифов]\n</COMP>\n</NOBODY>\n');
 
 
 
---
+++ patches/billing/db/1000314.sql
@@ -0,0 +1,2 @@
+INSERT INTO `Clauses` (`AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `IsPublish`, `Text`) VALUES (100, 100, 'Contracts/Enclosures/Types/Tariffs/Content', 'Тарифы', 'no', 'yes', 'yes', 'yes', '<NOBODY>\n <COMP path="Clauses/TariffList" args="NotUsed">\n [список тарифов]\n</COMP>\n</NOBODY>\n');
+
