c7e306a2faf1fc47cfb9f2b1ce98216eb35b0279
Alex Keda <admin@lissyara.su>
2012-09-30 19:23:39 +0400

    JBS-109: update user's pay limits

--- patches/hosting/db/1000245.sql
+++ patches/hosting/db/1000245.sql
@@ -1,3 +1,12 @@
 
 ALTER TABLE `Services` ADD `IsConditionally` ENUM('no','yes') NOT NULL DEFAULT 'no' AFTER `IsProlong`;
+-- SEPARATOR
+UPDATE `Users` SET `LayPayMaxDays` = 31;
+-- SEPARATOR
+UPDATE `Users` SET `LayPayThreshold` = 200;
+-- SEPARATOR
+UPDATE `Users` SET `LayPayMaxSumm`=((SELECT SUM(`Summ`) FROM `InvoicesOwners` WHERE `InvoicesOwners`.`UserID`=`Users`.`ID`) / 10);
+-- SEPARATOR
+UPDATE `Users` SET `LayPayMaxSumm` = 0 WHERE `LayPayMaxSumm` IS NULL;
+
 
