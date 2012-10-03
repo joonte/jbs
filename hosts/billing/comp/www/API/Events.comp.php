20a2530ab40c18b330adee42139a3d642d540dfa
Alex Keda <admin@lissyara.su>
2012-10-04 00:07:35 +0400

    JBS-478: use IsAdmin variable

--- hosts/billing/comp/www/API/Events.comp.php
+++ hosts/billing/comp/www/API/Events.comp.php
@@ -37,67 +37,55 @@ if($TimeResult){
 if(Is_Error(System_Load('modules/Authorisation.mod','libs/Tree.php')))
   return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
-$Entrance = Tree_Entrance('Groups',3000000);
-#-------------------------------------------------------------------------------
-switch(ValueOf($Entrance)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    return ERROR | @Trigger_Error(400);
-  case 'array':
-    #---------------------------------------------------------------------------
-    $Where = Array('UNIX_TIMESTAMP() - 10 <= `CreateDate`');
-    #---------------------------------------------------------------------------
-    $LastID = CacheManager::get($LastIDCacheID);
-    if($LastID){
-      Debug("[comp/www/API/Events]: last selected ID, from cache = " . $LastID . "; user = " . $GLOBALS['__USER']['ID']);
-      $Where[] = SPrintF('`ID` > %u',$LastID);
-    }
-    #---------------------------------------------------------------------------
-    $__USER = $GLOBALS['__USER'];
-    #---------------------------------------------------------------------------
-    if(!In_Array($__USER['GroupID'],$Entrance))
-      $Where[] = SPrintF('`UserID` = %u',$__USER['ID']);
-    #---------------------------------------------------------------------------
-    $Events = DB_Select('Events',Array('ID','Text',"(SELECT CONCAT(FROM_UNIXTIME(`CreateDate`,'%Y-%m-%d / %H:%i:%s / '),`Name`,' [',`Email`,']') FROM `Users` WHERE `Users`.`ID` = `Events`.`UserID`) as `UserInfo`",'PriorityID'),Array('SortOn'=>'ID','Where'=>$Where));
-    #---------------------------------------------------------------------------
-    switch(ValueOf($Events)){
-      case 'error':
-        return ERROR | @Trigger_Error(500);
-      case 'exception':
-        $Out = Array('Status'=>'Empty');
-	break;
-      case 'array':
-        #-----------------------------------------------------------------------
-        $Result = Array();
-        #-----------------------------------------------------------------------
-        foreach($Events as $Event){
-	  $Event['Text'] = HtmlSpecialChars($Event['Text']);
-          $Result[] = $Event;
-	  $LastID = $Event['ID'];
-	  #Debug("[comp/www/API/Events]: last selected ID = " . $LastID);
-	}
-        #-----------------------------------------------------------------------
-	CacheManager::add($LastIDCacheID,$LastID,24 * 3600); /* на сутки в кэш */
-	#-----------------------------------------------------------------------
-        $Out = Array('Status'=>'Ok','Events'=>$Result);
-	break;
-      default:
-        return ERROR | @Trigger_Error(101);
-    }
-    #---------------------------------------------------------------------------
-    #---------------------------------------------------------------------------
-    CacheManager::add($TimeCacheID,Time(),10);
-    CacheManager::add($OutCacheID,$Out,10);
-    #Debug("[comp/www/API/Events]: результат добавлен в кэш");
-    #---------------------------------------------------------------------------
-    #---------------------------------------------------------------------------
-    return $Out;
-    #---------------------------------------------------------------------------
-  default:
-    return ERROR | @Trigger_Error(101);
+#---------------------------------------------------------------------------
+$Where = Array('UNIX_TIMESTAMP() - 10 <= `CreateDate`');
+#---------------------------------------------------------------------------
+$LastID = CacheManager::get($LastIDCacheID);
+if($LastID){
+  Debug("[comp/www/API/Events]: last selected ID, from cache = " . $LastID . "; user = " . $GLOBALS['__USER']['ID']);
+  $Where[] = SPrintF('`ID` > %u',$LastID);
 }
+#---------------------------------------------------------------------------
+$__USER = $GLOBALS['__USER'];
+#---------------------------------------------------------------------------
+if(!$__USER['IsAdmin'])
+  $Where[] = SPrintF('`UserID` = %u',$__USER['ID']);
+#---------------------------------------------------------------------------
+$Events = DB_Select('Events',Array('ID','Text',"(SELECT CONCAT(FROM_UNIXTIME(`CreateDate`,'%Y-%m-%d / %H:%i:%s / '),`Name`,' [',`Email`,']') FROM `Users` WHERE `Users`.`ID` = `Events`.`UserID`) as `UserInfo`",'PriorityID'),Array('SortOn'=>'ID','Where'=>$Where));
+#---------------------------------------------------------------------------
+switch(ValueOf($Events)){
+case 'error':
+  return ERROR | @Trigger_Error(500);
+case 'exception':
+  $Out = Array('Status'=>'Empty');
+  break;
+case 'array':
+  #-----------------------------------------------------------------------
+  $Result = Array();
+  #-----------------------------------------------------------------------
+  foreach($Events as $Event){
+    $Event['Text'] = HtmlSpecialChars($Event['Text']);
+    $Result[] = $Event;
+    $LastID = $Event['ID'];
+    #Debug("[comp/www/API/Events]: last selected ID = " . $LastID);
+  }
+  #-----------------------------------------------------------------------
+  CacheManager::add($LastIDCacheID,$LastID,24 * 3600); /* на сутки в кэш */
+  #-----------------------------------------------------------------------
+  $Out = Array('Status'=>'Ok','Events'=>$Result);
+  break;
+default:
+  return ERROR | @Trigger_Error(101);
+}
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+CacheManager::add($TimeCacheID,Time(),10);
+CacheManager::add($OutCacheID,$Out,10);
+#Debug("[comp/www/API/Events]: результат добавлен в кэш");
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+return $Out;
+#---------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-
 
 ?>
