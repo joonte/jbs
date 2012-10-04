a7c089b870e07f5b0751b6bf8640e1290aa24417
lissyara <admin@lissyara.su>
2012-10-04 10:01:39 +0400

    JBS-109: fix exception

--- hosts/billing/comp/Tasks/GC/NotifyConditionallyInvoice.comp.php
+++ hosts/billing/comp/Tasks/GC/NotifyConditionallyInvoice.comp.php
@@ -23,107 +23,108 @@ case 'error':
 case 'exception':
   break;
 case 'array':
-  break;
-default:
-  return ERROR | @Trigger_Error(101);
-}
-#---------------------------------------------------------------------------
-foreach($Invoices as $Invoice){
-  # added by lissyara 2012-09-30 in 20:20 MSK, for JBS-109
-  # перебираем всех юзеров с условными инвойсами, смотрим сколько от статуса
-  # если от статуса больше 31 дня:
-  if($Invoice['StatusDate'] < Time() - 31*24*3600){
-    #---------------------------------------------------------------------------
-    # 1. откатываем инвойс в статус "Удалён"
-    $Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'Invoices','StatusID'=>'Rejected','RowsIDs'=>$Invoice['ID'],'Comment'=>'Отмена условно оплаченного счёта'));
-    #---------------------------------------------------------------------------
-    switch(ValueOf($Comp)){
-    case 'error':
-      return ERROR | @Trigger_Error(500);
-    case 'exception':
-      return $StatusSet;
-    case 'array':
-      # No more...
-      break;
-    default:
-      return ERROR | @Trigger_Error(101);
-    }
-    #---------------------------------------------------------------------------
-    # 2. вычитаем сумму счёта из договора, на который счёт.
-    $Contract = DB_Select('ContractsOwners','Balance',Array('UNIQ','ID'=>$Invoice['ContractID']));
-    switch(ValueOf($Contract)){
-    case 'error':
-      return ERROR | @Trigger_Error(500);
-    case 'exception':
-      return ERROR | @Trigger_Error(400);
-    case 'array':
-      break;
-    default:
-      return ERROR | @Trigger_Error(101);
-    }
-    #---------------------------------------------------------------------------
-    $After = $Contract['Balance'] - $Invoice['Summ'];
-    #---------------------------------------------------------------------------
-    $IsUpdate = DB_Update('Contracts',Array('Balance'=>$After),Array('ID'=>$Invoice['ContractID']));
-    if(Is_Error($IsUpdate))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    # заносим запись в историю операций с контрактами
-     $Comp = Comp_Load('Formats/Invoice/Number',$Invoice['ID']);
-     if(Is_Error($Comp))
-       return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    $IPosting = Array(
-                      #---------------------------------------------------------
-                     'ContractID' => $Invoice['ContractID'],
-                     'ServiceID'  => 2000,
-                     'Comment'    => SPrintF('Возврат средств условно зачисленных по счёту #%u',$Comp),
-                     'Before'     => $Contract['Balance'],
-                     'After'      => $After
-                     );
-    #-------------------------------------------------------------------
-    $PostingID = DB_Insert('Postings',$IPosting);
-    if(Is_Error($PostingID))
-      return ERROR | @Trigger_Error(500);
-    #-------------------------------------------------------------------
-    # 3. если балланс получился отрицательный - лочим все услуги этого договора
-    if($After < 0){
-      $Columns = Array('ID','ServiceID',
-                       '(SELECT `Services`.`Code` FROM `Services` WHERE `OrdersOwners`.`ServiceID` = `Services`.`ID`) AS `OrderTypeCode`'
-                       );
-      #-----------------------------------------------------------------
-      #$Orders = DB_Select('OrdersOwners',$Columns,Array('Where'=>SPrintF('`StatusID` = "Active" AND `ContractID` = %u',$Invoice['ContractID'])));
-      $Orders = DB_Select('OrdersOwners',$Columns,Array('Where'=>SPrintF('`StatusID` = "Active" AND `UserID` = %u',$Invoice['UserID'])));
-      switch(ValueOf($Orders)){
+  #---------------------------------------------------------------------------
+  foreach($Invoices as $Invoice){
+    # added by lissyara 2012-09-30 in 20:20 MSK, for JBS-109
+    # перебираем всех юзеров с условными инвойсами, смотрим сколько от статуса
+    # если от статуса больше 31 дня:
+    if($Invoice['StatusDate'] < Time() - 31*24*3600){
+      #---------------------------------------------------------------------------
+      # 1. откатываем инвойс в статус "Удалён"
+      $Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'Invoices','StatusID'=>'Rejected','RowsIDs'=>$Invoice['ID'],'Comment'=>'Отмена условно оплаченного счёта'));
+      #---------------------------------------------------------------------------
+      switch(ValueOf($Comp)){
       case 'error':
         return ERROR | @Trigger_Error(500);
       case 'exception':
-        break;
+        return $StatusSet;
       case 'array':
-        foreach($Orders as $Order){
-          # как-то дохрена сложно получается...
-	  # проще может событие вешать... и отдавать на откуп администратору.
-	  # подумать надо.
-	  Debug(SPrintF('[comp/Tasks/GC/NotifyConditionallyInvoice]: Необходимо залочить услугу (%s), номер заказа (#%u)',$Order['OrderTypeCode'],$Order['ID']));
-          #-----------------------------------------------------------------
-          $Event = Array(
+        # No more...
+        break;
+      default:
+        return ERROR | @Trigger_Error(101);
+      }
+      #---------------------------------------------------------------------------
+      # 2. вычитаем сумму счёта из договора, на который счёт.
+      $Contract = DB_Select('ContractsOwners','Balance',Array('UNIQ','ID'=>$Invoice['ContractID']));
+      switch(ValueOf($Contract)){
+      case 'error':
+        return ERROR | @Trigger_Error(500);
+      case 'exception':
+        return ERROR | @Trigger_Error(400);
+      case 'array':
+        break;
+      default:
+        return ERROR | @Trigger_Error(101);
+      }
+      #---------------------------------------------------------------------------
+      $After = $Contract['Balance'] - $Invoice['Summ'];
+      #---------------------------------------------------------------------------
+      $IsUpdate = DB_Update('Contracts',Array('Balance'=>$After),Array('ID'=>$Invoice['ContractID']));
+      if(Is_Error($IsUpdate))
+        return ERROR | @Trigger_Error(500);
+      #---------------------------------------------------------------------------
+      # заносим запись в историю операций с контрактами
+       $Comp = Comp_Load('Formats/Invoice/Number',$Invoice['ID']);
+       if(Is_Error($Comp))
+         return ERROR | @Trigger_Error(500);
+      #---------------------------------------------------------------------------
+      $IPosting = Array(
+                        #---------------------------------------------------------
+                       'ContractID' => $Invoice['ContractID'],
+                       'ServiceID'  => 2000,
+                       'Comment'    => SPrintF('Возврат средств условно зачисленных по счёту #%u',$Comp),
+                       'Before'     => $Contract['Balance'],
+                       'After'      => $After
+                       );
+      #-------------------------------------------------------------------
+      $PostingID = DB_Insert('Postings',$IPosting);
+      if(Is_Error($PostingID))
+        return ERROR | @Trigger_Error(500);
+      #-------------------------------------------------------------------
+      # 3. если балланс получился отрицательный - лочим все услуги этого договора
+      if($After < 0){
+        $Columns = Array('ID','ServiceID',
+                         '(SELECT `Services`.`Code` FROM `Services` WHERE `OrdersOwners`.`ServiceID` = `Services`.`ID`) AS `OrderTypeCode`'
+                         );
+        #-----------------------------------------------------------------
+        #$Orders = DB_Select('OrdersOwners',$Columns,Array('Where'=>SPrintF('`StatusID` = "Active" AND `ContractID` = %u',$Invoice['ContractID'])));
+        $Orders = DB_Select('OrdersOwners',$Columns,Array('Where'=>SPrintF('`StatusID` = "Active" AND `UserID` = %u',$Invoice['UserID'])));
+        switch(ValueOf($Orders)){
+        case 'error':
+          return ERROR | @Trigger_Error(500);
+        case 'exception':
+          break;
+        case 'array':
+          foreach($Orders as $Order){
+            # как-то дохрена сложно получается...
+            # проще может событие вешать... и отдавать на откуп администратору.
+            # подумать надо.
+             Debug(SPrintF('[comp/Tasks/GC/NotifyConditionallyInvoice]: Необходимо залочить услугу (%s), номер заказа (#%u)',$Order['OrderTypeCode'],$Order['ID']));
+            #-----------------------------------------------------------------
+            $Event = Array(
                         'UserID'        => $Invoice['UserID'],
                         'PriorityID'    => 'Warning',
                         'Text'          => SPrintF('Условно оплаченный счёт не был оплачен 31 день. Необходимо заблокировать услугу (%s), номер заказа (#%u)',$Order['OrderTypeCode'],$Order['ID']),
                         'IsReaded'      => FALSE
                         );
-          $Event = Comp_Load('Events/EventInsert',$Event);
-          if(!$Event)
-            return ERROR | @Trigger_Error(500);
-          }
-	  #-----------------------------------------------------------------
-        break;
-      default:
-        return ERROR | @Trigger_Error(101);
+            $Event = Comp_Load('Events/EventInsert',$Event);
+            if(!$Event)
+              return ERROR | @Trigger_Error(500);
+            }
+	    #-----------------------------------------------------------------
+          break;
+        default:
+          return ERROR | @Trigger_Error(101);
+        }
+      #-----------------------------------------------------------------
       }
-    #-----------------------------------------------------------------
     }
   }
+  #-----------------------------------------------------------------
+  break;
+default:
+  return ERROR | @Trigger_Error(101);
 }
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
