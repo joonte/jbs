bd3a50c27469d1cc1ad276eac2591d971c78278b
lissyara <admin@lissyara.su>
2012-10-15 10:04:17 +0400

    JBS-378: log user delete

--- hosts/billing/comp/Triggers/OnDelete/Users.comp.php
+++ hosts/billing/comp/Triggers/OnDelete/Users.comp.php
@@ -13,11 +13,37 @@ Eval(COMP_INIT);
 if($User['ID'] < 2001)
   return new gException('USER_CAN_NOT_DELETED',SPrintF('Пользователь [%s] не может быть удален, поскольку он системный',$User['Email']));
 #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 // удаляем события этого юзера
 $Where = SPrintF('`UserID` = %u',$User['ID']);
 $IsDelete = DB_Delete('Events',Array('Where'=>$Where));
 if(Is_Error($IsDelete))
 	return new gException('USERs_EVENTS_CAN_NOT_DELETED',SPrintF('Не удалось удалить события пользователя [%s]',$User['Email']));
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$Personal = DB_Select('Users',Array('ID','Email'),Array('UNIQ','ID'=>$GLOBALS['__USER']['ID']));
+#-------------------------------------------------------------------------------
+switch(ValueOf($Personal)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	return ERROR | @Trigger_Error(400);
+case 'array':
+	# No more...
+	break;
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
+$Event = Array(
+		'UserID'        => 1,
+		'PriorityID'    => 'Billing',
+		'Text'          => SPrintF('Соотрудником %s удалён пользователь %s',$Personal['Email'],$User['Email'])
+		);
+
+$Event = Comp_Load('Events/EventInsert',$Event);
+if(!$Event)
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 return TRUE;
 #-------------------------------------------------------------------------------
