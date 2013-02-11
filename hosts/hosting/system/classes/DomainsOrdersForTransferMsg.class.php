78f371b5777ed1a6cae1c25a8424ea54467d8c17
Alex Keda <admin@lissyara.su>
2013-02-11 23:10:18 +0400

    JBS-602: add new text

--- hosts/hosting/system/classes/DomainsOrdersForTransferMsg.class.php
+++ hosts/hosting/system/classes/DomainsOrdersForTransferMsg.class.php
@@ -24,128 +24,134 @@
         $UserID = DB_Select('DomainsOrdersOwners', 'UserID as ID', Array('UNIQ', 'Where' => $Where));
         $SchemeName = DB_Select('DomainsSchemes', 'Name as SchemeName', Array('UNIQ', 'ID' => $this->params['SchemeID']));
         #-------------------------------------------------------------------------------
-        if(IsSet($Registrar) && IsSet($PrefixRegistrar)){
-          $this->params['registrar'] = $Registrar;
-          $this->params['prefixRegistrar'] = $PrefixRegistrar;
+        #-------------------------------------------------------------------------------
+        if(In_Array($SchemeName['SchemeName'],Array('ru','su','рф'))){
+          #-------------------------------------------------------------------------------
+          if(IsSet($Registrar) && IsSet($PrefixRegistrar)){
+            $this->params['registrar'] = $Registrar;
+            $this->params['prefixRegistrar'] = $PrefixRegistrar;
 
-          # Получаем параметры регистратора к которому осуществляется трансфер
-          $RegistratorID = DB_Select('DomainsSchemes','RegistratorID as ID',Array('UNIQ','ID'=>$this->params['SchemeID']));
-          $Settings = DB_Select('Registrators','*',Array('UNIQ','ID'=>$RegistratorID['ID']));
-          #-----------------------------------------------------------------------------
-          Debug("[Notifies/Email/DomainsOrdersForTransfer]: Registrators[TypeID] - " . $Settings['TypeID']);
-          Debug("[Notifies/Email/DomainsOrdersForTransfer]: Settings[PrefixNic] - " . $Settings['PrefixNic']);
-          Debug("[Notifies/Email/DomainsOrdersForTransfer]: Registrar - " . $Registrar);
-          Debug("[Notifies/Email/DomainsOrdersForTransfer]: PrefixRegistrar - " . $PrefixRegistrar);
-          #-----------------------------------------------------------------------------
-          # Проверяем является ли регистрар нашим регистратором
-          if($PrefixRegistrar == $Settings['PrefixNic']){
-            $this->params['internalRegister'] = true;
-            #---------------------------------------------------------------------------
-            Debug("[Notifies/Email/DomainsOrdersForTransfer]: IsOurRegistrar - TRUE");
-            Debug("[Notifies/Email/DomainsOrdersForTransfer]: Инструкция по трансферу в пределах регистратора");
-            #---------------------------------------------------------------------------
-            $this->params['registratorID'] = $Settings['TypeID'];
-            $this->params['partnerLogin'] = $Settings['PartnerLogin'];
-            $this->params['partnerContract'] = $Settings['PartnerContract'];
+            # Получаем параметры регистратора к которому осуществляется трансфер
+            $RegistratorID = DB_Select('DomainsSchemes','RegistratorID as ID',Array('UNIQ','ID'=>$this->params['SchemeID']));
+            $Settings = DB_Select('Registrators','*',Array('UNIQ','ID'=>$RegistratorID['ID']));
+            #-----------------------------------------------------------------------------
+            Debug("[Notifies/Email/DomainsOrdersForTransfer]: Registrators[TypeID] - " . $Settings['TypeID']);
+            Debug("[Notifies/Email/DomainsOrdersForTransfer]: Settings[PrefixNic] - " . $Settings['PrefixNic']);
+            Debug("[Notifies/Email/DomainsOrdersForTransfer]: Registrar - " . $Registrar);
+            Debug("[Notifies/Email/DomainsOrdersForTransfer]: PrefixRegistrar - " . $PrefixRegistrar);
+            #-----------------------------------------------------------------------------
+            # Проверяем является ли регистрар нашим регистратором
+            if($PrefixRegistrar == $Settings['PrefixNic']){
+              $this->params['internalRegister'] = true;
+              #---------------------------------------------------------------------------
+              Debug("[Notifies/Email/DomainsOrdersForTransfer]: IsOurRegistrar - TRUE");
+              Debug("[Notifies/Email/DomainsOrdersForTransfer]: Инструкция по трансферу в пределах регистратора");
+              #---------------------------------------------------------------------------
+              $this->params['registratorID'] = $Settings['TypeID'];
+              $this->params['partnerLogin'] = $Settings['PartnerLogin'];
+              $this->params['partnerContract'] = $Settings['PartnerContract'];
 
-            # Достаем статью с информацией о шаблонах документов и контактами регистратора
-            $Where = SPrintF('`Partition`=\'Registrators/%s/internal\'', $PrefixRegistrar);
-            $Clause = DB_Select('Clauses','*',Array('UNIQ','Where'=>$Where));
-            switch(ValueOf($Clause)) {
-              case 'array':
-                $TransferDoc = trim(Strip_Tags($Clause['Text']));
+              # Достаем статью с информацией о шаблонах документов и контактами регистратора
+              $Where = SPrintF('`Partition`=\'Registrators/%s/internal\'', $PrefixRegistrar);
+              $Clause = DB_Select('Clauses','*',Array('UNIQ','Where'=>$Where));
+              switch(ValueOf($Clause)) {
+                case 'array':
+                  $TransferDoc = trim(Strip_Tags($Clause['Text']));
+                  break;
+                default:
+                  #-----------------------------------------------------------------------
+                  Debug(SPrintF('[Notifies/Email/DomainsOrdersForTransfer]: Статья не найдена. Ожидалась Registrators/%s/internal', $PrefixRegistrar));
+                  #-----------------------------------------------------------------------
+                  $TransferDoc = "Для получения информации об оформлении писем Вашему текущему регистратору и его контактах перейдите на его сайт.";
+                  #-----------------------------------------------------------------------
+                  # Уведомление об ошибке статьи
+                  $Event = Array(
+                      'UserID'    => $UserID['ID'],
+                      'PriorityID'=> 'Error',
+                      'Text'      => SPrintF('Статья по переносу домена не найдена. Ожидалась Registrators/%s/internal',$PrefixRegistrar),
+                      'IsReaded'  => FALSE
+                  );
 
-                break;
-              default:
-                #-----------------------------------------------------------------------
-                Debug(SPrintF('[Notifies/Email/DomainsOrdersForTransfer]: Статья не найдена. Ожидалась Registrators/%s/internal', $PrefixRegistrar));
-                #-----------------------------------------------------------------------
-                $TransferDoc = "Для получения информации об оформлении писем Вашему текущему регистратору и его контактах перейдите на его сайт.";
-                #-----------------------------------------------------------------------
-                # Уведомление об ошибке статьи
-                $Event = Array(
-                    'UserID'    => $UserID['ID'],
-                    'PriorityID'=> 'Error',
-                    'Text'      => SPrintF('Статья по переносу домена не найдена. Ожидалась Registrators/%s/internal',$PrefixRegistrar),
-                    'IsReaded'  => FALSE
-                );
+                  $Event = Comp_Load('Events/EventInsert', $Event);
+                  if(!$Event)
+                    return ERROR | @Trigger_Error(500);
+              }
 
-                $Event = Comp_Load('Events/EventInsert', $Event);
-                if(!$Event)
-                  return ERROR | @Trigger_Error(500);
+              $this->params['transferDoc'] = $TransferDoc;
             }
+            else {
+              $this->params['internalRegister'] = false;
+              #---------------------------------------------------------------------------
+              Debug("[Notifies/Email/DomainsOrdersForTransfer]: Инструкция по трансферу от стороннего регистратора");
+              #---------------------------------------------------------------------------
+              # Формируем постфикс идентификатора
+              switch ($SchemeName['SchemeName']){
+                case 'su':
+                  $PostfixNic = 'FID';
+                break;
+                case 'рф':
+                  $PostfixNic = 'RF';
+                break;
+                default:
+                  $PostfixNic = 'RIPN';
+              }
 
-            $this->params['transferDoc'] = $TransferDoc;
+              $this->params['registrar'] = $Registrar;
+              $this->params['registratorID'] = $Settings['TypeID'];
+              $this->params['jurName'] = $Settings['JurName'];
+              $this->params['prefixNic'] = $Settings['PrefixNic'];
+              $this->params['postfixNic'] = $PostfixNic;
+              $this->params['schemeName'] = strtoupper($SchemeName['SchemeName']);
+
+              # Достаем статью с информацией о шаблонах документов и контактами регистратора
+              $Where = SPrintF('`Partition`=\'Registrators/%s/external\'',$PrefixRegistrar);
+              $Clause = DB_Select('Clauses','*',Array('UNIQ','Where'=>$Where));
+              switch(ValueOf($Clause)){
+                case 'array':
+                  $TransferDoc = trim(Strip_Tags($Clause['Text']));
+                  break;
+                default:
+                  #-----------------------------------------------------------------------
+                  Debug(SPrintF('[Notifies/Email/DomainsOrdersForTransfer]: Статья не найдена. Ожидалась Registrators/%s/external',$PrefixRegistrar));
+                  #-----------------------------------------------------------------------
+                  $TransferDoc = "\n\nДля получения информации об оформлении писем Вашему текущему регистратору и его контактах перейдите на его сайт.";
+                  #-----------------------------------------------------------------------
+                  # Уведомление об ошибке статьи
+              $Event = Array(
+                      'UserID'	=> $UserID['ID'],
+                      'PriorityID'	=> 'Error',
+                      'Text'		=> SPrintF('Статья по переносу домена не найдена. Ожидалась Registrators/%s/external',$PrefixRegistrar),
+                      'IsReaded'	=> FALSE
+                            );
+                  $Event = Comp_Load('Events/EventInsert',$Event);
+                  if(!$Event)
+                    return ERROR | @Trigger_Error(500);
+                  #-----------------------------------------------------------------------
+              }
+
+              $this->params['transferDoc'] = $TransferDoc;
+            }
           }
           else {
-            $this->params['internalRegister'] = false;
-            #---------------------------------------------------------------------------
-            Debug("[Notifies/Email/DomainsOrdersForTransfer]: Инструкция по трансферу от стороннего регистратора");
-            #---------------------------------------------------------------------------
-            # Формируем постфикс идентификатора
-            switch ($SchemeName['SchemeName']){
-              case 'su':
-                $PostfixNic = 'FID';
-              break;
-              case 'рф':
-                $PostfixNic = 'RF';
-              break;
-              default:
-                $PostfixNic = 'RIPN';
-            }
+            Debug("[Notifies/Email/DomainsOrdersForTransfer]: Registrar или PrefixRegistrar не был определён.");
 
-            $this->params['registrar'] = $Registrar;
-            $this->params['registratorID'] = $Settings['TypeID'];
-            $this->params['jurName'] = $Settings['JurName'];
-            $this->params['prefixNic'] = $Settings['PrefixNic'];
-            $this->params['postfixNic'] = $PostfixNic;
-            $this->params['schemeName'] = strtoupper($SchemeName['SchemeName']);
+            # Уведомление об ошибке формирования инструкции
+            $Event = Array (
+                  'UserID'    => $UserID['ID'],
+                  'PriorityID'=> 'Error',
+                  'Text'      => SPrintF('Ошибка автоматического формирования инструкции по переносу домена (%s.%s) к нам.', $this->params['DomainName'], $SchemeName['SchemeName'] /*$this->params['SchemeName']*/),
+                  'IsReaded'  => FALSE
+            );
 
-            # Достаем статью с информацией о шаблонах документов и контактами регистратора
-            $Where = SPrintF('`Partition`=\'Registrators/%s/external\'',$PrefixRegistrar);
-            $Clause = DB_Select('Clauses','*',Array('UNIQ','Where'=>$Where));
-            switch(ValueOf($Clause)){
-              case 'array':
-                $TransferDoc = trim(Strip_Tags($Clause['Text']));
-                break;
-              default:
-                #-----------------------------------------------------------------------
-                Debug(SPrintF('[Notifies/Email/DomainsOrdersForTransfer]: Статья не найдена. Ожидалась Registrators/%s/external',$PrefixRegistrar));
-                #-----------------------------------------------------------------------
-                $TransferDoc = "\n\nДля получения информации об оформлении писем Вашему текущему регистратору и его контактах перейдите на его сайт.";
-                #-----------------------------------------------------------------------
-                # Уведомление об ошибке статьи
-            $Event = Array(
-                    'UserID'	=> $UserID['ID'],
-                    'PriorityID'	=> 'Error',
-                    'Text'		=> SPrintF('Статья по переносу домена не найдена. Ожидалась Registrators/%s/external',$PrefixRegistrar),
-                    'IsReaded'	=> FALSE
-                          );
-                $Event = Comp_Load('Events/EventInsert',$Event);
-                if(!$Event)
-                  return ERROR | @Trigger_Error(500);
-                #-----------------------------------------------------------------------
-            }
-
-            $this->params['transferDoc'] = $TransferDoc;
+            $Event = Comp_Load('Events/EventInsert', $Event);
+            if(!$Event)
+              return ERROR | @Trigger_Error(500);
           }
+        }else{
+          // буржуйские домены, пеерносятся через AuthInfo
+          $this->params['notUSSR'] = TRUE;
+          $this->params['schemeName'] = strtoupper($SchemeName['SchemeName']);
         }
-        else {
-          Debug("[Notifies/Email/DomainsOrdersForTransfer]: Registrar или PrefixRegistrar не был определён.");
-
-          # Уведомление об ошибке формирования инструкции
-          $Event = Array (
-                'UserID'    => $UserID['ID'],
-                'PriorityID'=> 'Error',
-                'Text'      => SPrintF('Ошибка автоматического формирования инструкции по переносу домена (%s.%s) к нам.', $this->params['DomainName'], $SchemeName['SchemeName'] /*$this->params['SchemeName']*/),
-                'IsReaded'  => FALSE
-          );
-
-          $Event = Comp_Load('Events/EventInsert', $Event);
-          if(!$Event)
-            return ERROR | @Trigger_Error(500);
-        }
-
         return $this->params;
      }
  }
