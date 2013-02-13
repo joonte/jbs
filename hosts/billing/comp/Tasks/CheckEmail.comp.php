97ec018732fc0b20b6207e449316e38eccfd3d96
Alex Keda <admin@lissyara.su>
2013-02-14 00:23:40 +0400

    JBS-609: add class

--- hosts/billing/comp/Tasks/CheckEmail.comp.php
+++ hosts/billing/comp/Tasks/CheckEmail.comp.php
@@ -19,9 +19,100 @@ if(!$Settings['IsActive'])
 	return 3600;
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
+if(Is_Error(System_Load('classes/ImapMailbox.php')))
+	return ERROR | @Trigger_Error(500);
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 $GLOBALS['TaskReturnInfo'] = Array();
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
+$mailbox = new ImapMailbox(SPrintF("{%s/pop3:110/notls}INBOX",$Settings['CheckEmailServer']), $Settings['CheckEmailLogin'], $Settings['CheckEmailPassword']);
+$mails = array();
+#-------------------------------------------------------------------------------
+foreach($mailbox->searchMailbox() as $mailId) {
+	$mail = $mailbox->getMail($mailId);
+	// $mailbox->setMailAsSeen($mail->mId);
+	// $mailbox->deleteMail($mail->mId);
+	$mails[] = $mail;
+}
+#-------------------------------------------------------------------------------
+Debug(SPrintF('[comp/Tasks/CheckEmail]: imap_num_msg = %s',SizeOf($mails)));
+$GLOBALS['TaskReturnInfo'][] = SPrintF('%s messages',SizeOf($mails));
+#-------------------------------------------------------------------------------
+foreach ($mails as $mail){
+	#-------------------------------------------------------------------------------
+	$Subject = $mail->subject;
+	$fromAddress = $mail->fromAddress;
+	$textPlain = $mail->textPlain;
+	#$GLOBALS['TaskReturnInfo'][] = SPrintF('fromAddress = %s',$fromAddress);
+	#-------------------------------------------------------------------------------
+	# достаём все заголовки
+	$References = FALSE;
+	#-------------------------------------------------------------------------------
+	$Headers = Explode("\n", Trim($mailbox->fetchHeader($mail->mId)));
+	#-------------------------------------------------------------------------------
+	if(Is_Array($Headers) && Count($Headers)){
+		foreach($Headers as $Line){
+			$HeaderLine = Explode(" ",Trim($Line));
+			#-------------------------------------------------------------------------------
+			if(StrToLower($HeaderLine[0]) == 'in-reply-to:')
+				$References = $HeaderLine[1];
+			#-------------------------------------------------------------------------------
+			if(StrToLower($HeaderLine[0]) == 'references:')
+				$References = $HeaderLine[1];
+			#-------------------------------------------------------------------------------
+		}
+	}
+	#-------------------------------------------------------------------------------
+	# проверяем наличие ссылки на тикет
+	if($References){
+		#-------------------------------------------------------------------------------
+		$Address = MailParse_RFC822_Parse_Addresses($References);
+		$Address = Explode("@",$Address[0]['address']);
+		#-------------------------------------------------------------------------------
+		if(IsSet($Address[1]) && $Address[1] == HOST_ID && IntVal($Address[0]) == $Address[0]){
+			#-------------------------------------------------------------------------------
+			Debug(SPrintF('[comp/Tasks/CheckEmail]: в Message-ID найдено число = %s',$Address[0]));
+			# проверяем наличие такого тикета
+			$Count = DB_Count('EdesksMessagesOwners',Array('ID'=>$Address[0]));
+			if(Is_Error($Count))
+				return ERROR | @Trigger_Error(500);
+			#-------------------------------------------------------------------------------
+			if($Count){
+				$MessageID = $Address[0];
+				Debug(SPrintF('[comp/Tasks/CheckEmail]: найден ответ на сообщение тикета %s',$MessageID));
+			}
+		}
+	}
+	#-------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------
+	# имеем 2 ситуации, задан или не задан $MessageID - соответственно, добавление в тикет или создание тикета
+	if(IsSet($MessageID)){
+		#-------------------------------------------------------------------------------
+		# сообщение на www/API/TicketMessageEdit
+		# перезадать $GLOBALS['__USER']['ID'], потом вернуть
+	}else{
+		#-------------------------------------------------------------------------------
+		# сообщение на www/API/TicketEdit, от юзера "Гость" (проверить его существование)
+	}
+	#-------------------------------------------------------------------------------
+	# ампутируем переменную, чтоб в один тикет не напостило все письма
+	UnSet($MessageID);
+}
+
+
+
+#Debug(SPrintF('[comp/Tasks/CheckEmail]: %s',print_r($mails,true)));
+
+
+#Debug(SPrintF('[comp/Tasks/CheckEmail]: %s',print_r($mailbox->fetchHeader(2),true)));
+
+
+
+
+
+
+return 120;
 $MBox = Imap_Open(SPrintF("{%s/pop3:110/notls}INBOX",$Settings['CheckEmailServer']), $Settings['CheckEmailLogin'], $Settings['CheckEmailPassword']);
 #-------------------------------------------------------------------------------
 $Count = imap_num_msg($MBox);
@@ -37,10 +128,28 @@ $GLOBALS['TaskReturnInfo'][] = SPrintF('%s messages',$Count);
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 # достаём заголовки сообщения
-$Headers = Explode("\n", Trim(Imap_FetchHeader($MBox, 1)));
+$MsgNum = 2;
+
+$References = FALSE;
+
+$Header = Imap_HeaderInfo($MBox, $MsgNum);
+if(IsSet($Header->references))
+	$References = $Header->references;
+if(IsSet($Header->in_reply_to))
+	$References = $Header->references;
+
+$GLOBALS['TaskReturnInfo'][] = SPrintF('References = %s',$References);
+
+Debug(print_r(imap_headerinfo($MBox, $MsgNum),true));
+Debug(print_r(Imap_FetchHeader($MBox, $MsgNum),true));
+return 20;
+
+
+$Headers = Explode("\n", Trim(Imap_FetchHeader($MBox, $MsgNum)));
+Debug('[comp/Tasks/CheckEmail]: after headers');
 if (Is_Array($Headers) && Count($Headers)){
 	foreach($Headers as $Line){
-		list($HeaderName,$HeaderContent) = Explode(" ",$Line);
+		list($HeaderName,$HeaderContent) = @Explode(" ",Trim($Line));
 		# проверяем References:
 		if(StrToLower($HeaderName) == 'references:'){
 			Debug(SPrintF('[comp/Tasks/CheckEmail]: %s',Trim($Line)));
@@ -57,18 +166,19 @@ if (Is_Array($Headers) && Count($Headers)){
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 # достаём тушку сообщения
-$Structure = Imap_FetchStructure($MBox, 1);
+$Structure = Imap_FetchStructure($MBox, $MsgNum);
 if(!empty($Structure->parts)){
 	for ($i = 0, $j = Count($Structure->parts); $i < $j; $i++){
 		$Part = $Structure->parts[$i];
 		if($Part->subtype == 'PLAIN'){
-			$Body = Imap_FetchBody($MBox, 1, $i+1);
+			$Body = Imap_FetchBody($MBox, $MsgNum, $i+1);
 		}
 	}
 }else{
-	$Body = Imap_Body($MBox, 1);
+	$Body = Imap_Body($MBox, $MsgNum);
 }
 #$GLOBALS['TaskReturnInfo'][] = print_r($Body,true);
+$GLOBALS['TaskReturnInfo'][] = print_r(Imap_Body($MBox, $MsgNum),true);
 
 
 #$MsgList = Imap_Headers($MBox);
