3ecd64a97cbfc09f4ed2df4b10d0c5802d5d5d30
lissyara <admin@lissyara.su>
2013-03-04 15:04:42 +0400

    JBS-637: add delete file to trigger

--- hosts/billing/comp/Triggers/OnDelete/Services.comp.php
+++ hosts/billing/comp/Triggers/OnDelete/Services.comp.php
@@ -1,6 +1,5 @@
 <?php
 
-
 #-------------------------------------------------------------------------------
 /** @author Великодный В.В. (Joonte Ltd.) */
 /******************************************************************************/
@@ -10,22 +9,30 @@ $__args_list = Array('Service');
 Eval(COMP_INIT);
 /******************************************************************************/
 /******************************************************************************/
+if(Is_Error(System_Load('libs/Upload.php')))
+	return ERROR | @Trigger_Error(500);
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 if($Service['IsProtected'])
-  return new gException('SERVICE_IS_PROTECTED',SPrintF('Услуга (%s) защищена и не может быть удалена',$Service['Name']));
+	return new gException('SERVICE_IS_PROTECTED',SPrintF('Услуга (%s) защищена и не может быть удалена',$Service['Name']));
 #-------------------------------------------------------------------------------
 $Count = DB_Count('Orders',Array('Where'=>SPrintF('`ServiceID` = %u',$Service['ID'])));
 if(Is_Error($Count))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 if($Count)
-  return new gException('SERVICE_ORDERS_EXISTS',SPrintF('Услуга (%s) не может быть удалена, т.к. на нее существуют заказы',$Service['Name']));
+	return new gException('SERVICE_ORDERS_EXISTS',SPrintF('Услуга (%s) не может быть удалена, т.к. на нее существуют заказы',$Service['Name']));
 #-------------------------------------------------------------------------------
 $Count = DB_Count('InvoicesItems',Array('Where'=>SPrintF('`ServiceID` = %u',$Service['ID'])));
 if(Is_Error($Count))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 if($Count)
-  return new gException('SERVICE_INVOICES_EXISTS',SPrintF('Услуга (%s) не может быть удалена, т.к. на были выписаны счета',$Service['Name']));
+	return new gException('SERVICE_INVOICES_EXISTS',SPrintF('Услуга (%s) не может быть удалена, т.к. на были выписаны счета',$Service['Name']));
+#-------------------------------------------------------------------------------
+if(!DeleteUploadedFile('Services',$Service['ID']))
+	return new gException('CANNOT_DELETE_FILE','Не удалось удалить связанный файл');
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 return TRUE;
 #-------------------------------------------------------------------------------
