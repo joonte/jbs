be9e6b134c5ea91ad3690e6f2b87c82292588820
lissyara <admin@lissyara.su>
2013-03-29 16:35:44 +0400

    JBS-454: move code to component

--- hosts/hosting/comp/Triggers/Statuses/ISPswOrders/Waiting.comp.php
+++ hosts/hosting/comp/Triggers/Statuses/ISPswOrders/Waiting.comp.php
@@ -40,61 +40,27 @@ default:
 }
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-# масив для вставки/обновления таблицы StatusesHistory
-$IOrdersHistory = Array('StatusDate'=>Time());
+$Params = Array(
+		'SchemeID'	=> $ISPswOrder['SchemeID'],
+		'OrderID'	=> $ISPswOrder['OrderID'],
+		'MaxOrders'	=> $Scheme['MaxOrders'],
+		'ServiceID'	=> 51000,
+		'ServiceName'	=> $Service['Name'],
+		'SchemeName'	=> $Scheme['Name']
+		);
 #-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-# проверяем, есть ли такой заказ в таблице с историей
-$Where = Array(SPrintF('`UserID` = %u',$UserID),'`ServiceID` = 51000',SPrintF('`SchemeID` = %u',$ISPswOrder['SchemeID']),SPrintF('`OrderID` = %u',$ISPswOrder['OrderID']));
-#-------------------------------------------------------------------------------
-$OrdersHistory = DB_Select('OrdersHistory',Array('ID'),Array('UNIQ','Where'=>$Where));
-switch(ValueOf($OrdersHistory)){
+$Comp = Comp_Load('Services/Orders/OrdersHistory',$Params);
+switch(ValueOf($Comp)){
 case 'error':
 	return ERROR | @Trigger_Error(500);
 case 'exception':
-	#-------------------------------------------------------------------------------
-	# проверяем, были ли такие же заказы
-	$Count = DB_Count('OrdersHistory',Array('Where'=>Array(SPrintF('`UserID` = %u',$UserID),'`ServiceID` = 51000',SPrintF('`SchemeID` = %u',$ISPswOrder['SchemeID']))));
-	if(Is_Error($Count))
-		return ERROR | Trigger_Error(500);
-	#-------------------------------------------------------------------------------
-	# проверяем, как много таких заказов можно делать
-	if($Scheme['MaxOrders'] > 0 && $Count >= $Scheme['MaxOrders'])
-		return new gException('TOO_MANY_ORDERS',SPrintF('Для данного тарифного плана существует ограничение на максимальное число заказов, равное %s. Ранее, вы уже делали заказы по данному тарифу, и больше сделать не можете. Выберите другой тарифный план.',$Scheme['MaxOrders']));
-	#-------------------------------------------------------------------------------
-	# вносим заказ в таблицу, если его там нет
-	$IOrdersHistory['UserID']	= $UserID;
-	$IOrdersHistory['Email']	= $GLOBALS['__USER']['Email'];
-	$IOrdersHistory['ServiceID']	= 51000;
-	$IOrdersHistory['ServiceName']	= $Service['Name'];
-	$IOrdersHistory['SchemeID']	= $ISPswOrder['SchemeID'];
-	$IOrdersHistory['SchemeName']	= $Scheme['Name'];
-	$IOrdersHistory['OrderID']	= $ISPswOrder['OrderID'];
-	$IOrdersHistory['CreateDate']	= Time();
-	#-------------------------------------------------------------------------------
-	$IsInsert = DB_Insert('OrdersHistory',$IOrdersHistory);
-	if(Is_Error($IsInsert))
-		return ERROR | @Trigger_Error(500);
-	#-------------------------------------------------------------------------------
-	break;
-	#-------------------------------------------------------------------------------
+	return $Comp;
 case 'array':
-	#-------------------------------------------------------------------------------
-	# это вторичная проставка статуса для заказа. просто обновляем StatusDate
-	$IsUpdate = DB_Update('OrdersHistory',$IOrdersHistory,Array('ID'=>$OrdersHistory['ID']));
-	if(Is_Error($IsUpdate))
-		return ERROR | @Trigger_Error(500);
-	#-------------------------------------------------------------------------------
-	break;
-	#-------------------------------------------------------------------------------
+	return TRUE;
 default:
 	return ERROR | @Trigger_Error(101);
 }
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-return TRUE;
-#-------------------------------------------------------------------------------
-
-
 
 ?>
