e916fef9f1aca5573057ed99d2c6324afab9604b
Alex Keda <admin@lissyara.su>
2013-05-29 21:42:21 +0400

    JBS-695: buttons examples

--- db/billing/db.sql
+++ db/billing/db.sql
@@ -2897,6 +2897,11 @@ INSERT INTO `Clauses` (`GroupID`, `AuthorID`, `EditorID`, `Partition`, `Title`, 
 INSERT INTO `Clauses` (`GroupID`,`AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `IsPublish`, `Text`) VALUES (1,100, 100, 'Contracts/Enclosures/Types/Tariffs/Content', 'Тарифы', 'yes', 'yes', 'yes', 'yes', '<NOBODY>\n <COMP path="Clauses/TariffList" args="NotUsed">\n [список тарифов]\n</COMP>\n</NOBODY>\n');
 
 
+-- SEPARATOR
+/* added by lissyara, 2013-05-29 in 21:38 MSK, for JBS-695 */
+INSERT INTO `Clauses` (`GroupID`, `AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `IsPublish`, `Text`) VALUES (11, 100, 100, '01:Info.gif', 'Ваш запрос принят в работу', 'no', 'yes', 'no', 'yes', 'Здравствуйте.<br />\nВаш запрос принят в работу.<br />\nКак только по нему появится новая информация, вам будет об этом сообщено в данном тикете.');
+-- SEPARATOR
+INSERT INTO `Clauses` (`GroupID`, `AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `IsPublish`, `Text`) VALUES (11, 100, 100, '03:Dollar.gif', 'Запрос передан в бухгалтерию', 'no', 'yes', 'no', 'yes', '<p>\n Ваш запрос передан ответственному сотруднику в бухгалтерии.\n</p>\n<p>\n Обратите внимание, что бухгалтерия работает только по рабочим дням, с 9 до 18 часов.\n</p>');
 
 
 UNLOCK TABLES;
