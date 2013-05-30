12312a49e07563d331312984bdada6d50b011f4d
Alex Keda <admin@lissyara.su>
2013-05-30 10:40:39 +0400

    JBS-698: add examples

---
+++ patches/hosting/db/1000275.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `Clauses` (`GroupID`, `AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `IsPublish`, `Text`) VALUES (11, 100, 100, '01:Info.gif', 'Ваш запрос принят в работу', 'no', 'yes', 'no', 'yes', 'Здравствуйте.<br />\nВаш запрос принят в работу.<br />\nКак только по нему появится новая информация, вам будет об этом сообщено в данном тикете.');
+-- SEPARATOR
+INSERT INTO `Clauses` (`GroupID`, `AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `IsPublish`, `Text`) VALUES (11, 100, 100, '03:Dollar.gif', 'Запрос передан в бухгалтерию', 'no', 'yes', 'no', 'yes', '<p>\n Ваш запрос передан ответственному сотруднику в бухгалтерии.\n</p>\n<p>\n Обратите внимание, что бухгалтерия работает только по рабочим дням, с 9 до 18 часов.\n</p>');
+
+
