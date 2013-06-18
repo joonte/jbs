602e618ca9ac1ef9893ca399f406efa3cb6733b0
Alex Keda <admin@lissyara.su>
2013-06-18 09:00:47 +0400

    JBS-94: fix error, if order expired

--- hosts/hosting/comp/www/Administrator/API/OrderRestore.comp.php
+++ hosts/hosting/comp/www/Administrator/API/OrderRestore.comp.php
@@ -47,77 +47,91 @@ case 'error':
 case 'exception':
 	return ERROR | @Trigger_Error(400);
 case 'array':
-	#-------------------------------------------------------------------------------
-	$OrderConsiders = DB_Select('OrdersConsider',Array('ID','DaysReserved','DaysRemainded','`DaysRemainded`*`Cost`*(1-`Discont`) as `SummRemainded`'),Array('Where'=>SPrintF('`OrderID` = %u AND `DaysRemainded` > 0',$Order['OrderID'])));
-	#-------------------------------------------------------------------------------
-	switch(ValueOf($OrderConsiders)){
-	case 'error':
-		return ERROR | @Trigger_Error(500);
-	case 'exception':
-		return ERROR | @Trigger_Error(400);
-	case 'array':
-		#-------------------------------------------------------------------------------
-		foreach($OrderConsiders as $OrderConsider){
-			#-------------------------------------------------------------------------------
-			$SummRemainded = (double)$OrderConsider['SummRemainded'];
-			#-------------------------------------------------------------------------------
-			if($SummRemainded){
-				#-------------------------------------------------------------------------------
-				$Comp = Comp_Load('Formats/Order/Number',$Order['OrderID']);
-				if(Is_Error($Comp))
-					return ERROR | @Trigger_Error(500);
-				#-------------------------------------------------------------------------------
-				$IsUpdate = Comp_Load('www/Administrator/API/PostingMake',Array('ContractID'=>$Order['ContractID'],'Summ'=>$SummRemainded,'ServiceID'=>3000,'Comment'=>SPrintF('Услуга "%s", #%s',$Service['NameShort'],$Comp)));
-				#-------------------------------------------------------------------------------
-				switch(ValueOf($IsUpdate)){
-				case 'error':
-					return ERROR | @Trigger_Error(500);
-				case 'exception':
-					return ERROR | @Trigger_Error(400);
-				case 'array':
-					#-------------------------------------------------------------------------------
-					$IsUpdate = DB_Update('OrdersConsider',Array('DaysReserved'=>($OrderConsider['DaysReserved'] - $OrderConsider['DaysRemainded']),'DaysRemainded'=>0,'DaysConsidered'=>0),Array('ID'=>$OrderConsider['ID']));
-					if(Is_Error($IsUpdate))
-						return ERROR | @Trigger_Error(500);
-					#-------------------------------------------------------------------------------
-					break;
-					#-------------------------------------------------------------------------------
-				default:
-					return ERROR | @Trigger_Error(101);
-				}
-				#-------------------------------------------------------------------------------
-				#-------------------------------------------------------------------------------
-				$Comp = Comp_Load('Formats/Currency',$SummRemainded);
-				if(Is_Error($Comp))
-					return ERROR | @Trigger_Error(500);
-				#-------------------------------------------------------------------------------
-				$Event = Array(
-						'UserID'        => $Order['UserID'],
-						'PriorityID'    => 'Hosting',
-						'Text'          => SPrintF('Осуществлён возврат средств за заказ (#%u), услуга (%s), сумма (%s)',$OrderID,$Service['NameShort'],$Comp)
-						);
-				$Event = Comp_Load('Events/EventInsert',$Event);
-				if(!$Event)
-					return ERROR | @Trigger_Error(500);
-				#-------------------------------------------------------------------------------
-			}
-			#-------------------------------------------------------------------------------
-		}
-		#-------------------------------------------------------------------------------
-		if(Is_Error(DB_Commit($TransactionID)))
-			return ERROR | @Trigger_Error(500);
-		#-------------------------------------------------------------------------------
-		#---------------------------END TRANSACTION-------------------------------------
-		return Array('Status'=>'Ok');
-		#-------------------------------------------------------------------------------
-	default:
-		return ERROR | @Trigger_Error(101);
-	}
-	#-------------------------------------------------------------------------------
+	break;
 default:
 	return ERROR | @Trigger_Error(101);
 }
 #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$OrderConsiders = DB_Select('OrdersConsider',Array('ID','DaysReserved','DaysRemainded','`DaysRemainded`*`Cost`*(1-`Discont`) as `SummRemainded`'),Array('Where'=>SPrintF('`OrderID` = %u AND `DaysRemainded` > 0',$Order['OrderID'])));
+#-------------------------------------------------------------------------------
+switch(ValueOf($OrderConsiders)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	#-------------------------------------------------------------------------------
+	# ничё нету, значит надо собрать свой массив
+	$OrderConsiders = Array(Array('ID'=>0,'DaysReserved'=>0,'DaysRemainded'=>0,'SummRemainded'=>0));
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+case 'array':
+	break;
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+foreach($OrderConsiders as $OrderConsider){
+	#-------------------------------------------------------------------------------
+	$SummRemainded = (double)$OrderConsider['SummRemainded'];
+	#-------------------------------------------------------------------------------
+	if($SummRemainded){
+		#-------------------------------------------------------------------------------
+		$Comp = Comp_Load('Formats/Order/Number',$Order['OrderID']);
+		if(Is_Error($Comp))
+			return ERROR | @Trigger_Error(500);
+		#-------------------------------------------------------------------------------
+		$IsUpdate = Comp_Load('www/Administrator/API/PostingMake',Array('ContractID'=>$Order['ContractID'],'Summ'=>$SummRemainded,'ServiceID'=>3000,'Comment'=>SPrintF('Услуга "%s", #%s',$Service['NameShort'],$Comp)));
+		#-------------------------------------------------------------------------------
+		switch(ValueOf($IsUpdate)){
+		case 'error':
+			return ERROR | @Trigger_Error(500);
+		case 'exception':
+			return ERROR | @Trigger_Error(400);
+		case 'array':
+			#-------------------------------------------------------------------------------
+			if($OrderConsider['ID'] != 0){
+				#-------------------------------------------------------------------------------
+				$IsUpdate = DB_Update('OrdersConsider',Array('DaysReserved'=>($OrderConsider['DaysReserved'] - $OrderConsider['DaysRemainded']),'DaysRemainded'=>0,'DaysConsidered'=>0),Array('ID'=>$OrderConsider['ID']));
+				if(Is_Error($IsUpdate))
+					return ERROR | @Trigger_Error(500);
+				#-------------------------------------------------------------------------------
+			}
+			#-------------------------------------------------------------------------------
+			break;
+			#-------------------------------------------------------------------------------
+		default:
+			return ERROR | @Trigger_Error(101);
+		}
+		#-------------------------------------------------------------------------------
+		#-------------------------------------------------------------------------------
+		if($OrderConsider['ID'] != 0){
+			#-------------------------------------------------------------------------------
+			$Comp = Comp_Load('Formats/Currency',$SummRemainded);
+			if(Is_Error($Comp))
+				return ERROR | @Trigger_Error(500);
+			#-------------------------------------------------------------------------------
+			$Event = Array(
+					'UserID'        => $Order['UserID'],
+					'PriorityID'    => 'Hosting',
+					'Text'          => SPrintF('Осуществлён возврат средств за заказ (#%u), услуга (%s), сумма (%s)',$OrderID,$Service['NameShort'],$Comp)
+					);
+			$Event = Comp_Load('Events/EventInsert',$Event);
+			if(!$Event)
+				return ERROR | @Trigger_Error(500);
+			#-------------------------------------------------------------------------------
+		}
+		#-------------------------------------------------------------------------------
+	}
+	#-------------------------------------------------------------------------------
+}
+#-------------------------------------------------------------------------------
+if(Is_Error(DB_Commit($TransactionID)))
+	return ERROR | @Trigger_Error(500);
+#-------------------------------------------------------------------------------
+#---------------------------END TRANSACTION-------------------------------------
+return Array('Status'=>'Ok');
 #-------------------------------------------------------------------------------
 
 ?>
