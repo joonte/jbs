878c7c37ce1245513e9580b4583f768167ea917d
Alex Keda <admin@lissyara.su>
2013-06-27 20:00:16 +0400

    JBS-679: code clean

--- hosts/billing/comp/Tasks/GC/NotifyWaitingInvoice.comp.php
+++ hosts/billing/comp/Tasks/GC/NotifyWaitingInvoice.comp.php
@@ -1,6 +1,5 @@
 <?php
 
-
 #-------------------------------------------------------------------------------
 /** @author Sergey Sedov (for www.host-food.ru) */
 /******************************************************************************/
@@ -14,54 +13,64 @@ $Where = SPrintF("`StatusID` = 'Waiting' AND `StatusDate` BETWEEN (UNIX_TIMESTAM
 #-------------------------------------------------------------------------------
 $Invoices = DB_Select('InvoicesOwners',Array('ID','UserID'),Array('SortOn'=>'CreateDate', 'IsDesc'=>TRUE, 'Where'=>$Where));
 switch(ValueOf($Invoices)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    return TRUE;
-  case 'array':
-    #---------------------------------------------------------------------------
-    foreach($Invoices as $Invoice){
-      Debug( SPrintF("[Tasks/GC/NotifyWaitingInvoice]: Уведомление о неоплаченном счете #%d.",$Invoice['ID']) );
-      #----------------------------------TRANSACTION----------------------------
-      if(Is_Error(DB_Transaction($TransactionID = UniqID('comp/Tasks/GC/NotifyWaitingInvoice'))))
-        return ERROR | @Trigger_Error(500);
-      #-------------------------------------------------------------------------
-      $IsSend = NotificationManager::sendMsg(new Message('NotPayedInvoice', (integer)$Invoice['UserID'], Array('Theme'=>SPrintF('Неоплаченный счет #%d',$Invoice['ID']),'InvoiceID'=>$Invoice['ID'])));
-      #-------------------------------------------------------------------------
-      switch(ValueOf($IsSend)){
-      case 'true':
-        #-------------------------------------------------------------------------
-        $Event = Array(
-			'UserID'	=> $Invoice['UserID'],
-			'PriorityID'	=> 'Billing',
-			'Text'		=> SPrintF('Уведомление о неоплаченном счете #%d, неоплачен более %d дней',$Invoice['ID'],$Params['DaysBeforeNotice'])
-		      );
-	$Event = Comp_Load('Events/EventInsert',$Event);
-	if(!$Event)
-		return ERROR | @Trigger_Error(500);
-      break;
-      case 'exception':
-        #-------------------------------------------------------------------------
-        $Event = Array(
-			'UserID'	=> $Invoice['UserID'],
-			'PriorityID'	=> 'Billing',
-			'Text'		=> SPrintF('Уведомление о неоплаченном счете #%d не доставлено. Не удалось оповестить пользователя ни одним из методов.',$Invoice['ID'])
-		      );
-	$Event = Comp_Load('Events/EventInsert',$Event);
-	if(!$Event)
-		return ERROR | @Trigger_Error(500);
-      break;
-      default:
-        return ERROR | @Trigger_Error(500);
-      }
-      #-------------------------------------------------------------------------
-      if(Is_Error(DB_Commit($TransactionID)))
-        return ERROR | @Trigger_Error(500);
-      #-------------------------------------------------------------------------
-    }
-    return TRUE;
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	return TRUE;
+case 'array':
+	#---------------------------------------------------------------------------
+	foreach($Invoices as $Invoice){
+		Debug( SPrintF("[Tasks/GC/NotifyWaitingInvoice]: Уведомление о неоплаченном счете #%d.",$Invoice['ID']) );
+		#----------------------------------TRANSACTION----------------------------
+		if(Is_Error(DB_Transaction($TransactionID = UniqID('comp/Tasks/GC/NotifyWaitingInvoice'))))
+			return ERROR | @Trigger_Error(500);
+		#-------------------------------------------------------------------------
+		$IsSend = NotificationManager::sendMsg(new Message('NotPayedInvoice', (integer)$Invoice['UserID'], Array('Theme'=>SPrintF('Неоплаченный счет #%d',$Invoice['ID']),'InvoiceID'=>$Invoice['ID'])));
+		#-------------------------------------------------------------------------
+		switch(ValueOf($IsSend)){
+		case 'true':
+			#-------------------------------------------------------------------------
+			$Event = Array(
+					'UserID'	=> $Invoice['UserID'],
+					'PriorityID'	=> 'Billing',
+					'Text'		=> SPrintF('Уведомление о неоплаченном счете #%d, неоплачен более %d дней',$Invoice['ID'],$Params['DaysBeforeNotice'])
+					);
+			$Event = Comp_Load('Events/EventInsert',$Event);
+			#-------------------------------------------------------------------------------
+			if(!$Event)
+				return ERROR | @Trigger_Error(500);
+			#-------------------------------------------------------------------------------
+			break;
+			#-------------------------------------------------------------------------------
+		case 'exception':
+			#-------------------------------------------------------------------------
+			$Event = Array(
+					'UserID'	=> $Invoice['UserID'],
+					'PriorityID'	=> 'Billing',
+					'Text'		=> SPrintF('Уведомление о неоплаченном счете #%d не доставлено. Не удалось оповестить пользователя ни одним из методов.',$Invoice['ID'])
+					);
+			#-------------------------------------------------------------------------------
+			$Event = Comp_Load('Events/EventInsert',$Event);
+			if(!$Event)
+				return ERROR | @Trigger_Error(500);
+			#-------------------------------------------------------------------------------
+			break;
+			#-------------------------------------------------------------------------------
+		default:
+			return ERROR | @Trigger_Error(500);
+		}
+		#-------------------------------------------------------------------------
+		if(Is_Error(DB_Commit($TransactionID)))
+			return ERROR | @Trigger_Error(500);
+		#-------------------------------------------------------------------------
+	}
+	#-------------------------------------------------------------------------------
+	return TRUE;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
+#-------------------------------------------------------------------------------
+
 
 ?>
