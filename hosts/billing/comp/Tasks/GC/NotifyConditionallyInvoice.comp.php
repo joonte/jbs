7602270a606d8cd5378d02049d0388fad65fd01a
Alex Keda <admin@lissyara.su>
2013-07-06 19:41:26 +0400

    JBS-244: fix error

--- hosts/billing/comp/Tasks/GC/NotifyConditionallyInvoice.comp.php
+++ hosts/billing/comp/Tasks/GC/NotifyConditionallyInvoice.comp.php
@@ -207,7 +207,7 @@ case 'array':
 		#-------------------------------------------------------------------------------
 		# шлём юзеру уведомление
 		$UserBalance = $User;
-		$Summ = Comp_Load('Formats/Currency',$Users['Balance']);
+		$Summ = Comp_Load('Formats/Currency',$User['Balance']);
 		if(Is_Error($Summ))
 			return ERROR | @Trigger_Error(500);
 		$UserBalance['Balance'] = $Summ;
