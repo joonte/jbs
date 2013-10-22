97deb434a42a1ca6abdefc50138904bd7298a7bc
Alex Keda <admin@lissyara.su>
2013-10-23 00:22:34 +0400

    JBS-603: fix error

--- hosts/hosting/comp/Tasks/GC/DeleteDomainsForTransfer.comp.php
+++ hosts/hosting/comp/Tasks/GC/DeleteDomainsForTransfer.comp.php
@@ -34,43 +34,43 @@ default:
 #-------------------------------------------------------------------------------
 foreach($DomainOrders as $DomainOrder){
 	#-------------------------------------------------------------------------------
-	if($DomainOrder['StatusID'] == 'ForTransfer' || ($DomainOrder['StatusID'] == 'OnTransfer' && In_Array($DomainOrder['Name'],Array('ru','su','рф')))){
-		#-------------------------------------------------------------------------------
-		Debug(SPrintF("[Tasks/GC/DeleteDomainsForTransfer]: Удаление домена '%s.%s', статус '%s'",$DomainOrder['DomainName'],$DomainOrder['Name'],$DomainOrder['StatusID']));
-		#----------------------------------TRANSACTION----------------------------------
-		if(Is_Error(DB_Transaction($TransactionID = UniqID('comp/Tasks/GC/DeleteDomainsForTransfer'))))
-			return ERROR | @Trigger_Error(500);
-		#-------------------------------------------------------------------------------
-		$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'DomainsOrders','StatusID'=>'Deleted','RowsIDs'=>$DomainOrder['ID'],'Comment'=>SPrintF('Заказ домена не был перенесён к регистратору %s, более 180 дней',$DomainOrder['RegistratorName'])));
-		#-------------------------------------------------------------------------------
-		switch(ValueOf($Comp)){
-		case 'array':
-			#-------------------------------------------------------------------------------
-			$Event = Array(
-					'UserID'    => $DomainOrder['UserID'],
-					'PriorityID'=> 'Hosting',
-					'Text'      => SPrintF('Автоматическое удаление домена (%s.%s), находится в статусе "%s" более 180 дней',$DomainOrder['DomainName'],$DomainOrder['Name'],$DomainOrder['StatusID'])
-					);
-			$Event = Comp_Load('Events/EventInsert',$Event);
-			#-------------------------------------------------------------------------------
-			if(!$Event)
-				return ERROR | @Trigger_Error(500);
-			#-------------------------------------------------------------------------------
-			break;
-			#-------------------------------------------------------------------------------
-		default:
-			return ERROR | @Trigger_Error(500);
-		}
-		#-------------------------------------------------------------------------------
-		#-------------------------------------------------------------------------------
-		if(Is_Error(DB_Commit($TransactionID)))
-			return ERROR | @Trigger_Error(500);
-		#-------------------------------------------------------------------------------
-	}else{
+	if(!($DomainOrder['StatusID'] == 'ForTransfer' || ($DomainOrder['StatusID'] == 'OnTransfer' && In_Array($DomainOrder['Name'],Array('ru','su','рф'))))){
 		#-------------------------------------------------------------------------------
 		Debug(SPrintF("[Tasks/GC/DeleteDomainsForTransfer]: Домен не попал в условие: '%s.%s', статус: '%s'",$DomainOrder['DomainName'],$DomainOrder['Name'],$DomainOrder['StatusID']));
 		#-------------------------------------------------------------------------------
 	}
+	#-------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------
+	Debug(SPrintF("[Tasks/GC/DeleteDomainsForTransfer]: Удаление домена '%s.%s', статус '%s'",$DomainOrder['DomainName'],$DomainOrder['Name'],$DomainOrder['StatusID']));
+	#----------------------------------TRANSACTION----------------------------------
+	if(Is_Error(DB_Transaction($TransactionID = UniqID('comp/Tasks/GC/DeleteDomainsForTransfer'))))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'DomainsOrders','StatusID'=>'Deleted','RowsIDs'=>$DomainOrder['ID'],'Comment'=>SPrintF('Заказ домена не был перенесён к регистратору %s, более 180 дней',$DomainOrder['RegistratorName'])));
+	#-------------------------------------------------------------------------------
+	switch(ValueOf($Comp)){
+	case 'array':
+		#-------------------------------------------------------------------------------
+		$Event = Array(
+				'UserID'    => $DomainOrder['UserID'],
+				'PriorityID'=> 'Hosting',
+				'Text'      => SPrintF('Автоматическое удаление домена (%s.%s), находится в статусе "%s" более 180 дней',$DomainOrder['DomainName'],$DomainOrder['Name'],$DomainOrder['StatusID'])
+				);
+		$Event = Comp_Load('Events/EventInsert',$Event);
+		#-------------------------------------------------------------------------------
+		if(!$Event)
+			return ERROR | @Trigger_Error(500);
+		#-------------------------------------------------------------------------------
+		break;
+		#-------------------------------------------------------------------------------
+	default:
+		return ERROR | @Trigger_Error(500);
+	}
+	#-------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------
+	if(Is_Error(DB_Commit($TransactionID)))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
 }
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
