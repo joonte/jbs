514042d9ce3ecad68d0790f6a4493155510e4d89
Alex Keda <admin@lissyara.su>
2014-02-25 10:11:40 +0400

    JBS-607: code clean

--- hosts/billing/comp/www/Administrator/NoticeEdit.comp.php
+++ hosts/billing/comp/www/Administrator/NoticeEdit.comp.php
@@ -1,6 +1,5 @@
 <?php
 
-
 #-------------------------------------------------------------------------------
 /** @author Великодный В.В. (Joonte Ltd.) */
 /******************************************************************************/
@@ -14,120 +13,122 @@ $TableID =  (string) @$Args['TableID'];
 $RowID   = (integer) @$Args['RowID'];
 #-------------------------------------------------------------------------------
 if(Is_Error(System_Load('modules/Authorisation.mod','classes/DOM.class.php')))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $AdminNotice = DB_Select($TableID,Array('ID','AdminNotice'),Array('UNIQ','ID'=>$RowID));
 #-------------------------------------------------------------------------------
 switch(ValueOf($AdminNotice)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    return ERROR | @Trigger_Error(400);
-  case 'array':
-    #---------------------------------------------------------------------------
-    $DOM = new DOM();
-    #---------------------------------------------------------------------------
-    $Links = &Links();
-    # Коллекция ссылок
-    $Links['DOM'] = &$DOM;
-    #---------------------------------------------------------------------------
-    if(Is_Error($DOM->Load('Window')))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    $Script = new Tag('SCRIPT',Array('type'=>'text/javascript','src'=>'SRC:{Js/Pages/Administrator/NoticeEdit.js}'));
-    #---------------------------------------------------------------------------
-    $DOM->AddChild('Head',$Script);
-    #---------------------------------------------------------------------------
-    $DOM->AddText('Title','Редактирование заметки');
-    #---------------------------------------------------------------------------
-    $Table = Array();
-    #---------------------------------------------------------------------------
-    $Comp = Comp_Load(
-      'Form/TextArea',
-      Array(
-       'name'	=> 'AdminNotice',
-       'style'	=> 'width: 400px',
-       'rows'	=> 10
-      ),
-      $AdminNotice['AdminNotice']
-    );
-    if(Is_Error($Comp))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    $Table[] = $Comp;
-    #---------------------------------------------------------------------------
-    $Comp = Comp_Load(
-      'Form/Input',
-      Array(
-        'type'    => 'button',
-        'onclick' => "form.AdminNotice.value = '';NoticeEdit();",
-        'value'   => 'Удалить'
-      )
-    );
-    if(Is_Error($Comp))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    if(!$AdminNotice['AdminNotice'])
-      $Comp->AddAttribs(Array('disabled'=>'true'));
-    #---------------------------------------------------------------------------
-    $Div = new Tag('DIV',Array('align'=>'right'),$Comp);
-    #---------------------------------------------------------------------------
-    $Comp = Comp_Load(
-      'Form/Input',
-      Array(
-        'type'    => 'button',
-        'onclick' => 'NoticeEdit();',
-        'value'   => 'Изменить'
-      )
-    );
-    if(Is_Error($Comp))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    $Div->AddChild($Comp);
-    #---------------------------------------------------------------------------
-    $Table[] = $Div;
-    #---------------------------------------------------------------------------
-    $Comp = Comp_Load('Tables/Standard',$Table);
-    if(Is_Error($Comp))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    $Form = new Tag('FORM',Array('name'=>'NoticeEditForm','onsubmit'=>'return false;'),$Comp);
-    #---------------------------------------------------------------------------
-    $Comp = Comp_Load(
-      'Form/Input',
-      Array(
-        'name'  => 'TableID',
-        'type'  => 'hidden',
-        'value' => $TableID
-      )
-    );
-    if(Is_Error($Comp))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    $Form->AddChild($Comp);
-    #---------------------------------------------------------------------------
-    $Comp = Comp_Load(
-      'Form/Input',
-      Array(
-        'name'  => 'RowID',
-        'type'  => 'hidden',
-        'value' => $AdminNotice['ID']
-      )
-    );
-    if(Is_Error($Comp))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    $Form->AddChild($Comp);
-    #---------------------------------------------------------------------------
-    $DOM->AddChild('Into',$Form);
-    #---------------------------------------------------------------------------
-    if(Is_Error($DOM->Build(FALSE)))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    return Array('Status'=>'Ok','DOM'=>$DOM->Object);
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	return ERROR | @Trigger_Error(400);
+case 'array':
+	#-------------------------------------------------------------------------------
+	$DOM = new DOM();
+	#-------------------------------------------------------------------------------
+	$Links = &Links();
+	# Коллекция ссылок
+	$Links['DOM'] = &$DOM;
+	#-------------------------------------------------------------------------------
+	if(Is_Error($DOM->Load('Window')))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$Script = new Tag('SCRIPT',Array('type'=>'text/javascript','src'=>'SRC:{Js/Pages/Administrator/NoticeEdit.js}'));
+	#-------------------------------------------------------------------------------
+	$DOM->AddChild('Head',$Script);
+	#-------------------------------------------------------------------------------
+	$DOM->AddText('Title','Редактирование заметки');
+	#-------------------------------------------------------------------------------
+	$Table = Array();
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load(
+			'Form/TextArea',
+			Array(
+				'name'	=> 'AdminNotice',
+				'style'	=> 'width: 400px',
+				'rows'	=> 10
+				),
+			$AdminNotice['AdminNotice']
+			);
+	if(Is_Error($Comp))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$Table[] = $Comp;
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load(
+			'Form/Input',
+			Array(
+				'type'    => 'button',
+				'onclick' => "form.AdminNotice.value = '';NoticeEdit();",
+				'value'   => 'Удалить'
+				)
+			);
+	if(Is_Error($Comp))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	if(!$AdminNotice['AdminNotice'])
+		$Comp->AddAttribs(Array('disabled'=>'true'));
+	#-------------------------------------------------------------------------------
+	$Div = new Tag('DIV',Array('align'=>'right'),$Comp);
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load(
+			'Form/Input',
+			Array(
+				'type'    => 'button',
+				'onclick' => 'NoticeEdit();',
+				'value'   => 'Изменить'
+				)
+			);
+	if(Is_Error($Comp))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$Div->AddChild($Comp);
+	#-------------------------------------------------------------------------------
+	$Table[] = $Div;
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load('Tables/Standard',$Table);
+	if(Is_Error($Comp))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$Form = new Tag('FORM',Array('name'=>'NoticeEditForm','onsubmit'=>'return false;'),$Comp);
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load(
+			'Form/Input',
+			Array(
+				'name'  => 'TableID',
+				'type'  => 'hidden',
+				'value' => $TableID
+				)
+
+			);
+	if(Is_Error($Comp))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$Form->AddChild($Comp);
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load(
+			'Form/Input',
+			Array(
+				'name'  => 'RowID',
+				'type'  => 'hidden',
+				'value' => $AdminNotice['ID']
+			)
+		);
+	if(Is_Error($Comp))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$Form->AddChild($Comp);
+	#-------------------------------------------------------------------------------
+	$DOM->AddChild('Into',$Form);
+	#-------------------------------------------------------------------------------
+	if(Is_Error($DOM->Build(FALSE)))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	return Array('Status'=>'Ok','DOM'=>$DOM->Object);
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
 #-------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------
 ?>
