d7f73c3ed6c4414a7e7ee5867689ef486f5773ff
Alex Keda <admin@lissyara.su>
2014-04-02 11:39:29 +0400

    relase and small fixes

--- patches/hosting/db/1000286.sql
+++ patches/hosting/db/1000286.sql
@@ -4,6 +4,6 @@ ALTER TABLE `VPSOrders` ADD `IP` char(60) NULL AFTER `Login`;
 UPDATE `VPSOrders` SET `IP` = `Login` WHERE `StatusID` = 'Active' OR `StatusID` = 'Suspended';
 -- SEPARATOR
 ALTER TABLE `VPSSchemes` DROP `disktempl`;
-
+-- SEPARATOR
 ALTER TABLE `VPSSchemes` ADD `blkiotune` int(9) NOT NULL AFTER `maxdesc`;
 
