137892383059441087dc4b6241738db2a1e660fa
Alex Keda <admin@lissyara.su>
2015-02-19 13:42:12 +0300

    JBS-905: add action, add settings

--- hosts/billing/comp/Tasks/GC/ResetOrdersDays.comp.php
+++ hosts/billing/comp/Tasks/GC/ResetOrdersDays.comp.php
@@ -9,6 +9,14 @@ $__args_list = Array('Params');
 Eval(COMP_INIT);
 /******************************************************************************/
 /******************************************************************************/
+$Config = Config();
+#-------------------------------------------------------------------------------
+$Settings = $Config['Tasks']['Types']['GC']['ResetOrdersDaysSettings'];
+#-------------------------------------------------------------------------------
+if(!$Settings['IsActive'])
+	return TRUE;
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # находим всех сотрудников - их заказы не окучиваем
 $Users = DB_Select('Users',Array('ID','Name'),Array('Where'=>SPrintF("(SELECT `IsDepartment` FROM `Groups` WHERE `Groups`.`ID` = `Users`.`GroupID`) = 'yes' OR `ID` = 100")));
 #-------------------------------------------------------------------------------
@@ -19,11 +27,11 @@ case 'exception':
 	return ERROR | @Trigger_Error(400);
 case 'array':
 	#-------------------------------------------------------------------------------
-	$PersonalIDs = Array();
+	$ExcludeIDs = Array();
 	#-------------------------------------------------------------------------------
 	foreach($Users as $User)
-		if(!In_Array($User['ID'],$PersonalIDs))
-			$PersonalIDs[] = $User['ID'];
+		if(!In_Array($User['ID'],$ExcludeIDs))
+			$ExcludeIDs[] = $User['ID'];
 	#-------------------------------------------------------------------------------
 	break;
 	#-------------------------------------------------------------------------------
@@ -49,20 +57,19 @@ default:
 }
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-$UserIDs = Array();
-#-------------------------------------------------------------------------------
 foreach($OrdersConsider as $OrderConsider){
 	#-------------------------------------------------------------------------------
-	if($OrderConsider['SumDaysRemainded'] <= 28)
+	if($OrderConsider['SumDaysRemainded'] <= IntVal($Settings['ResetDaysTo']))
 		continue;
 	#-------------------------------------------------------------------------------
-	#Debug(SPrintF('[comp/Tasks/GC/ResetOrdersDays]: OrderID = %s; DaysRemainded = %s',$OrderConsider['OrderID'],$OrderConsider['SumDaysRemainded']));
+	$OrderID = $OrderConsider['OrderID'];
+	#Debug(SPrintF('[comp/Tasks/GC/ResetOrdersDays]: OrderID = %s; DaysRemainded = %s',$OrderID,$OrderConsider['SumDaysRemainded']));
 	#-------------------------------------------------------------------------------
 	#-------------------------------------------------------------------------------
 	# Заказы этого клиента, только те для которых есть учёт в таблице OrdersConsider
-	$Where = Array(SPrintF('`UserID` = (SELECT `UserID` FROM `OrdersOwners` WHERE `ID` = %u)',$OrderConsider['OrderID']),'(SELECT `OrderID` FROM `OrdersConsider` WHERE `OrdersConsider`.`OrderID` = `OrdersOwners`.`ID` LIMIT 1) IS NOT NULL');
+	$Where = Array(SPrintF('`UserID` = (SELECT `UserID` FROM `OrdersOwners` WHERE `ID` = %u)',$OrderID),'(SELECT `OrderID` FROM `OrdersConsider` WHERE `OrdersConsider`.`OrderID` = `OrdersOwners`.`ID` LIMIT 1) IS NOT NULL');
 	#-------------------------------------------------------------------------------
-	$Orders = DB_Select('OrdersOwners',Array('ID','UserID'),Array('Where'=>$Where));
+	$Orders = DB_Select('OrdersOwners',Array('ID','UserID','StatusID'),Array('Where'=>$Where));
 	#-------------------------------------------------------------------------------
 	switch(ValueOf($Orders)){
 	case 'error':
@@ -80,28 +87,32 @@ foreach($OrdersConsider as $OrderConsider){
 	#-------------------------------------------------------------------------------
 	$UserID = $Order['UserID'];
 	#-------------------------------------------------------------------------------
-	# если сотрудник - ничего не делаем
-	if(In_Array($UserID,$PersonalIDs))
+	# если сотрудник или исключение - ничего не делаем
+	if(In_Array($UserID,$ExcludeIDs))
 		continue;
 	#-------------------------------------------------------------------------------
-	if(In_Array($UserID,$UserIDs)){
-		#-------------------------------------------------------------------------------
-		#continue;
-		#-------------------------------------------------------------------------------
-	}else{
-		#-------------------------------------------------------------------------------
-		$UserIDs[] = $UserID;
-		#-------------------------------------------------------------------------------
-	}
 	#-------------------------------------------------------------------------------
 	$Array = Array();
 	#-------------------------------------------------------------------------------
-	foreach($Orders as $Order)
+	foreach($Orders as $Order){
+		#-------------------------------------------------------------------------------
+		if($Order['StatusID'] == 'Suspended'){
+			#-------------------------------------------------------------------------------
+			Debug(SPrintF('[comp/Tasks/GC/ResetOrdersDays]: UserID = %s; OrderID = %s в статусе Suspended, все заказы пользователя пропускаются',$UserID,$Order['ID']));
+			#-------------------------------------------------------------------------------
+			$ExcludeIDs[] = $UserID;
+			#-------------------------------------------------------------------------------
+			continue 2;
+			#-------------------------------------------------------------------------------
+		}
+		#-------------------------------------------------------------------------------
 		$Array[] = $Order['ID'];
+		#-------------------------------------------------------------------------------
+	}
 	#-------------------------------------------------------------------------------
 	#Debug(SPrintF('[comp/Tasks/GC/ResetOrdersDays]: OrderIDs = %s',Implode(',',$Array)));
 	#-------------------------------------------------------------------------------
-	# ищщем заказы за денежки
+	# ищщем, есть ли у него заказы за денежки
 	$Where = Array('`Cost` > 0','`Discont` < 1','`DaysRemainded` > 0',SPrintF('`OrderID` IN (%s)',Implode(',',$Array)));
 	#-------------------------------------------------------------------------------
 	$Count = DB_Count('OrdersConsider',Array('Where'=>$Where));
@@ -126,47 +137,42 @@ foreach($OrdersConsider as $OrderConsider){
 	if($Count)
 		continue;
 	#-------------------------------------------------------------------------------
+	Debug(SPrintF('[comp/Tasks/GC/ResetOrdersDays]: Надо списывать для заказa OrderID = %s; юзер = %s',$OrderID,$UserID));
 	#-------------------------------------------------------------------------------
-	Debug(SPrintF('[comp/Tasks/GC/ResetOrdersDays]: Надо списывать для заказa OrderID = %s; юзера = %s',$OrderConsider['OrderID'],$UserID));
-
-	#----------------------------------TRANSACTION----------------------------
-
-
-
-
-
-
-#      if(Is_Error(DB_Transaction($TransactionID = UniqID('comp/Tasks/GC/EraseDeletedInvoice'))))
-#        return ERROR | @Trigger_Error(500);
+	#----------------------------------TRANSACTION----------------------------------
+	if(Is_Error(DB_Transaction($TransactionID = UniqID('ResetOrdersDays'))))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------
+	# удаляем весь учёт для заказа, где скидка 100% или цена 0
+	$IsDelete = DB_Delete('OrdersConsider',Array('Where'=>SPrintF('`OrderID` = %u',$OrderID)));
+	if(Is_Error($IsDelete))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	# втыкаем новый учёт, на нужное число дней
+	$IOrdersConsider = Array('OrderID'=>$OrderID,'DaysReserved'=>IntVal($Settings['ResetDaysTo']),'Cost'=>0);
+	#-------------------------------------------------------------------------------
+	$OrdersConsiderID = DB_Insert('OrdersConsider',$IOrdersConsider);
+	if(Is_Error($OrdersConsiderID))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$IsUpdate = DB_Update('OrdersConsider',Array('DaysConsidered'=>0),Array('ID'=>$OrdersConsiderID));
+	if(Is_Error($IsUpdate))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	# снимаем галку про автопродление
+	$IsUpdate = DB_Update('Orders',Array('IsAutoProlong'=>FALSE),Array('ID'=>$OrderID));
+	if(Is_Error($IsUpdate))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------
+	if(Is_Error(DB_Commit($TransactionID)))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
 }
-#      #-------------------------------------------------------------------------
-#      $Comp = Comp_Load('www/API/Delete',Array('TableID'=>'Invoices','RowsIDs'=>$Invoice['ID']));
-#      #-------------------------------------------------------------------------
-#      switch(ValueOf($Comp)){
-#      case 'array':
-#        $Event = Array(
-#			'UserID'	=> $Invoice['UserID'],
-#			'PriorityID'	=> 'Billing',
-#			'Text'		=> SPrintF('Отменённый счёт #%d автоматически удалён, оплата не поступила в течение %d дней.',$Invoice['ID'],$Params['Invoices']['DaysBeforeErase'])
-#	              );
-#	$Event = Comp_Load('Events/EventInsert',$Event);
-#	if(!$Event)
-#		return ERROR | @Trigger_Error(500);
-#      break;
-#      default:
-#        return ERROR | @Trigger_Error(500);
-#      }
-#      #-------------------------------------------------------------------------
-#      if(Is_Error(DB_Commit($TransactionID)))
-#        return ERROR | @Trigger_Error(500);
-#      #-------------------------------------------------------------------------
-#    }
-#    $Count = DB_Count('Invoices',Array('Where'=>$Where));
-#    if(Is_Error($Count))
-#      return ERROR | @Trigger_Error(500);
-#    return ($Count?$Count:TRUE);
-#  default:
-#    return ERROR | @Trigger_Error(101);
-#}
-
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+return TRUE;
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 ?>
