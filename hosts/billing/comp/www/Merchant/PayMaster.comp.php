09a2cf1ac08f3175c7aee86b7e9e96c888f1fd07
Alex Keda <admin@lissyara.su>
2015-04-07 15:21:35 +0300

    JBS-272: add MTS

--- hosts/billing/comp/www/Merchant/PayMaster.comp.php
+++ hosts/billing/comp/www/Merchant/PayMaster.comp.php
@@ -74,7 +74,8 @@ $LMI_PAYMENT_SYSTEMs = Array(
 				81	=> 'Онлайн-банкинг',
 				82	=> 'Ростелеком',
 				92	=> 'Пластиковые карты',
-				93	=> 'Банковские карты'
+				93	=> 'Банковские карты',
+				102	=> 'МТС'
 			    );
 #-------------------------------------------------------------------------------
 $PS = SPrintF('%s / %s',((IsSet($LMI_PAYMENT_SYSTEMs[$Args['LMI_PAYMENT_SYSTEM']]))?$LMI_PAYMENT_SYSTEMs[$Args['LMI_PAYMENT_SYSTEM']]:'LMI_PAYMENT_SYSTEM'),$Args['LMI_PAYMENT_SYSTEM']);
