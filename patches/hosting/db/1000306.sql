697d3dfaf5f29ca1e428572d78e973458593706f
Alex Keda <admin@lissyara.su>
2015-04-25 15:56:14 +0300

    JBS-990: join *NoticeDelete tasks

---
+++ patches/hosting/db/1000306.sql
@@ -0,0 +1,7 @@
+
+DELETE FROM `Tasks` WHERE `ID` IN (53,57,69,73,77,83,93);
+
+-- SEPARATOR
+
+INSERT INTO `Tasks` (`ID`,`UserID`,`TypeID`,`Params`,`IsActive`) VALUES (53,1,'NoticeDelete','[]','yes');
+
