6fcdb3ecfd4cc2234d516f9cbd524175d50dc3d6
Alex Keda <admin@lissyara.su>
2015-05-27 14:28:55 +0300

    JBS-699: code clean

--- hosts/billing/comp/www/Rss/News.comp.php
+++ hosts/billing/comp/www/Rss/News.comp.php
@@ -1,6 +1,5 @@
 <?php
 
-
 #-------------------------------------------------------------------------------
 /** @author Великодный В.В. (Joonte Ltd.) */
 /******************************************************************************/
@@ -9,6 +8,10 @@
 Eval(COMP_INIT);
 /******************************************************************************/
 /******************************************************************************/
+Header('Content-Type: application/rss+xml');
+#-------------------------------------------------------------------------------
+echo "<" . "?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
+#-------------------------------------------------------------------------------
 $Rss = new Tag('rss',Array('version'=>'2.0'));
 #-------------------------------------------------------------------------------
 $Channel = new Tag('channel');
@@ -23,44 +26,42 @@ $Channel->AddChild(new Tag('managingEditor',$Email = SPrintF('rss@%s',HOST_ID)))
 $Versions = DB_Select('Clauses',Array('ID','PublicDate','Title','Text'),Array('Limits'=>Array(0,20),'Where'=>Array('`GroupID` = 2','`IsPublish` = "yes"'),'SortOn'=>'PublicDate','IsDesc'=>TRUE));
 #-------------------------------------------------------------------------------
 switch(ValueOf($Versions)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    #---------------------------------------------------------------------------
-    # no more...
-  break;
-  case 'array':
-    #---------------------------------------------------------------------------
-    foreach($Versions as $Version){
-       #------------------------------------------------------------------------
-       $Link = SPrintF('http://%s/Rss/New?NewID=%u',HOST_ID,$Version['ID']);
-       #------------------------------------------------------------------------
-       $Text = Strip_Tags($Version['Text']);
-       #------------------------------------------------------------------------
-       if(Mb_StrLen($Text) > 120)
-         $Text = SPrintF('%s...',Mb_SubStr($Text,0,120));
-       #------------------------------------------------------------------------
-       $Item = new Tag('item');
-       $Item->AddChild(new Tag('guid',$Link));
-       $Item->AddChild(new Tag('title',$Version['Title']));
-       $Item->AddChild(new Tag('description',$Text));
-       $Item->AddChild(new Tag('author',$Email));
-       $Item->AddChild(new Tag('pubDate',Date('r',$Version['PublicDate'])));
-       $Item->AddChild(new Tag('link',$Link));
-       #------------------------------------------------------------------------
-       $Channel->AddChild($Item);
-    }
-    #---------------------------------------------------------------------------
-    $Rss->AddChild($Channel);
-    #---------------------------------------------------------------------------
-    Header('Content-Type: application/rss+xml');
-    #---------------------------------------------------------------------------
-    echo "<" . "?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
-    #---------------------------------------------------------------------------
-    return $Rss->ToXMLString();
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	return $Rss->ToXMLString();
+case 'array':
+	break;
+default:
+	return ERROR | @Trigger_Error(101);
 }
+#-------------------------------------------------------------------------------
+foreach($Versions as $Version){
+	#-------------------------------------------------------------------------------
+	$Link = SPrintF('http://%s/Rss/New?NewID=%u',HOST_ID,$Version['ID']);
+	#-------------------------------------------------------------------------------
+	$Text = Strip_Tags($Version['Text']);
+	#-------------------------------------------------------------------------------
+	if(Mb_StrLen($Text) > 120)
+		$Text = SPrintF('%s...',Mb_SubStr($Text,0,120));
+	#-------------------------------------------------------------------------------
+	$Item = new Tag('item');
+	$Item->AddChild(new Tag('guid',$Link));
+	$Item->AddChild(new Tag('title',$Version['Title']));
+	$Item->AddChild(new Tag('description',$Text));
+	$Item->AddChild(new Tag('author',$Email));
+	$Item->AddChild(new Tag('pubDate',Date('r',$Version['PublicDate'])));
+	$Item->AddChild(new Tag('link',$Link));
+	#-------------------------------------------------------------------------------
+	$Channel->AddChild($Item);
+	#-------------------------------------------------------------------------------
+}
+#-------------------------------------------------------------------------------
+$Rss->AddChild($Channel);
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+return $Rss->ToXMLString();
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
 ?>
