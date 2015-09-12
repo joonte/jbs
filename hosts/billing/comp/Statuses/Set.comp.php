28fe9d725fa94a52ef732fdad5fc5dd1182bb053
Alex Keda <admin@lissyara.su>
2015-09-12 22:04:11 +0300

    JBS-1083: add Ctrl+Enter to forms

--- hosts/billing/comp/Statuses/Set.comp.php
+++ hosts/billing/comp/Statuses/Set.comp.php
@@ -15,7 +15,7 @@ $Template = &$Links[$LinkID];
 /******************************************************************************/
 /******************************************************************************/
 if($Template['Source']['Count'] < 1)
-  return FALSE;
+	return FALSE;
 #-------------------------------------------------------------------------------
 $Links['DOM']->AddChild('Head',new Tag('SCRIPT',Array('type'=>'text/javascript','src'=>'SRC:{Js/StatusSet.js}')));
 #-------------------------------------------------------------------------------
@@ -28,53 +28,46 @@ $Statuses = $Config['Statuses'][$ModeID];
 $Options = Array();
 #-------------------------------------------------------------------------------
 foreach(Array_Keys($Statuses) as $StatusID)
-  $Options[$StatusID] = $Statuses[$StatusID]['Name'];
+	$Options[$StatusID] = $Statuses[$StatusID]['Name'];
 #-------------------------------------------------------------------------------
 $Comp = Comp_Load('Form/Select',Array('name'=>'StatusID','style'=>'width: 100%;'),$Options);
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = Array('Статус',$Comp);
 #-------------------------------------------------------------------------------
-$Comp = Comp_Load('Form/Input',Array('name'=>'Comment','type'=>'text'));
+#-------------------------------------------------------------------------------
+$OnKeyPress = SPrintF("ctrlEnterEvent(event) && ShowConfirm('Вы подтверждаете установку статуса?',\"StatusSet('%s');\");",$ModeID);
+#-------------------------------------------------------------------------------
+$Comp = Comp_Load('Form/Input',Array('name'=>'Comment','type'=>'text','OnKeyPress'=>$OnKeyPress));
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = Array('Комментарий',$Comp);
 #-------------------------------------------------------------------------------
-$Comp = Comp_Load(
-  'Form/Input',
-  Array(
-    'onclick' => SPrintF("ShowConfirm('Вы подтверждаете установку статуса?',\"StatusSet('%s');\");",$ModeID),
-    'type'    => 'button',
-    'value'   => 'Установить'
-  )
-);
+#-------------------------------------------------------------------------------
+$Comp = Comp_Load('Form/Input',Array('onclick'=>SPrintF("ShowConfirm('Вы подтверждаете установку статуса?',\"StatusSet('%s');\");",$ModeID),'type'=>'button','value'=>'Установить'));
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = $Comp;
 #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 $Comp = Comp_Load('Tables/Standard',$Table);
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $NoBody = new Tag('NOBODY',$Comp);
 #-------------------------------------------------------------------------------
-$Comp = Comp_Load(
-  'Form/Input',
-  Array(
-    'name'  => 'ModeID',
-    'type'  => 'hidden',
-    'value' => $ModeID
-  )
-);
+$Comp = Comp_Load('Form/Input',Array('name'=>'ModeID','type'=>'hidden','value'=>$ModeID));
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $NoBody->AddChild($Comp);
 #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 return $NoBody;
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
 ?>
