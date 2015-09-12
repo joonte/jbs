b609c0a6dc49ceb4db7bdb1f98115ae2c2601fbc
Alex Keda <admin@lissyara.su>
2015-09-12 15:53:09 +0300

    JBS-1084: change favicon when user have unclosed tickets

--- hosts/billing/comp/www/Administrator/API/Appearanse.comp.php
+++ hosts/billing/comp/www/Administrator/API/Appearanse.comp.php
@@ -1,6 +1,5 @@
 <?php
 
-
 #-------------------------------------------------------------------------------
 /** @author Великодный В.В. (Joonte Ltd.) */
 /******************************************************************************/
@@ -13,135 +12,175 @@ $Args = Args();
 $Copyright = (string) @$Args['Copyright'];
 #-------------------------------------------------------------------------------
 if(Is_Error(System_Load('modules/Authorisation.mod','libs/Upload.php')))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Upload = Upload_Get('TopLogo');
 #-------------------------------------------------------------------------------
 switch(ValueOf($Upload)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    # No more...
-  break;
-  case 'array':
-    #---------------------------------------------------------------------------
-    $File = SPrintF('styles/%s/Images/TopLogo.png',HOST_ID);
-    #---------------------------------------------------------------------------
-    $IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
-    if(Is_Error($IsWrite))
-      return ERROR | @Trigger_Error(500);
-  break;
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	# No more...
+	break;
+case 'array':
+	#-------------------------------------------------------------------------------
+	$File = SPrintF('styles/%s/Images/TopLogo.png',HOST_ID);
+	#-------------------------------------------------------------------------------
+	$IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
+	if(Is_Error($IsWrite))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $Upload = Upload_Get('Favicon');
 #-------------------------------------------------------------------------------
 switch(ValueOf($Upload)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    # No more...
-  break;
-  case 'array':
-    #---------------------------------------------------------------------------
-    $File = SPrintF('styles/%s/Images/favicon.ico',HOST_ID);
-    #---------------------------------------------------------------------------
-    $IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
-    if(Is_Error($IsWrite))
-      return ERROR | @Trigger_Error(500);
-  break;
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	# No more...
+	break;
+case 'array':
+	#-------------------------------------------------------------------------------
+	$File = SPrintF('styles/%s/Images/favicon.ico',HOST_ID);
+	#-------------------------------------------------------------------------------
+	$IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
+	if(Is_Error($IsWrite))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$Upload = Upload_Get('FaviconPNG');
+#-------------------------------------------------------------------------------
+switch(ValueOf($Upload)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	# No more...
+	break;
+case 'array':
+	#-------------------------------------------------------------------------------
+	$File = SPrintF('styles/%s/Images/favicon.png',HOST_ID);
+	#-------------------------------------------------------------------------------
+	$IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
+	if(Is_Error($IsWrite))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $IsUpdate = DB_Update('Config',Array('Value'=>$Copyright),Array('Where'=>"`Param` = 'Copyright'"));
 if(Is_Error($IsUpdate))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Upload = Upload_Get('Logo');
 #-------------------------------------------------------------------------------
 switch(ValueOf($Upload)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    # No more...
-  break;
-  case 'array':
-    #---------------------------------------------------------------------------
-    $File = SPrintF('styles/%s/Images/Logo.bmp',HOST_ID);
-    #---------------------------------------------------------------------------
-    $IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
-    if(Is_Error($IsWrite))
-      return ERROR | @Trigger_Error(500);
-  break;
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	# No more...
+	break;
+case 'array':
+	#-------------------------------------------------------------------------------
+	$File = SPrintF('styles/%s/Images/Logo.bmp',HOST_ID);
+	#-------------------------------------------------------------------------------
+	$IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
+	if(Is_Error($IsWrite))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $Upload = Upload_Get('Stamp');
 #-------------------------------------------------------------------------------
 switch(ValueOf($Upload)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    # No more...
-  break;
-  case 'array':
-    #---------------------------------------------------------------------------
-    $File = SPrintF('styles/%s/Images/Stamp.bmp',HOST_ID);
-    #---------------------------------------------------------------------------
-    $IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
-    if(Is_Error($IsWrite))
-      return ERROR | @Trigger_Error(500);
-  break;
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	# No more...
+	break;
+case 'array':
+	#-------------------------------------------------------------------------------
+	$File = SPrintF('styles/%s/Images/Stamp.bmp',HOST_ID);
+	#-------------------------------------------------------------------------------
+	$IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
+	if(Is_Error($IsWrite))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $Upload = Upload_Get('dSign');
 #-------------------------------------------------------------------------------
 switch(ValueOf($Upload)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    # No more...
-  break;
-  case 'array':
-    #---------------------------------------------------------------------------
-    $File = SPrintF('styles/%s/Images/dSign.bmp',HOST_ID);
-    #---------------------------------------------------------------------------
-    $IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
-    if(Is_Error($IsWrite))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-  break;
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	# No more...
+	break;
+case 'array':
+	#-------------------------------------------------------------------------------
+	$File = SPrintF('styles/%s/Images/dSign.bmp',HOST_ID);
+	#-------------------------------------------------------------------------------
+	$IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
+	if(Is_Error($IsWrite))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $Upload = Upload_Get('aSign');
 #-------------------------------------------------------------------------------
 switch(ValueOf($Upload)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    # No more...
-  break;
-  case 'array':
-    #---------------------------------------------------------------------------
-    $File = SPrintF('styles/%s/Images/aSign.bmp',HOST_ID);
-    #---------------------------------------------------------------------------
-    $IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
-    if(Is_Error($IsWrite))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-  break;
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	# No more...
+	break;
+case 'array':
+	#-------------------------------------------------------------------------------
+	$File = SPrintF('styles/%s/Images/aSign.bmp',HOST_ID);
+	#-------------------------------------------------------------------------------
+	$IsWrite = IO_Write(SPrintF('%s/%s',SYSTEM_PATH,$File),$Upload['Data'],TRUE);
+	if(Is_Error($IsWrite))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
 #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 return Array('Status'=>'Ok');
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
 ?>
--- hosts/billing/comp/www/Administrator/Appearanse.comp.php
+++ hosts/billing/comp/www/Administrator/Appearanse.comp.php
@@ -8,7 +8,8 @@ Eval(COMP_INIT);
 /******************************************************************************/
 /******************************************************************************/
 if(Is_Error(System_Load('classes/DOM.class.php','modules/Authorisation.mod')))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $DOM = new DOM();
 #-------------------------------------------------------------------------------
@@ -17,11 +18,12 @@ $Links = &Links();
 $Links['DOM'] = &$DOM;
 #-------------------------------------------------------------------------------
 if(Is_Error($DOM->Load('Base')))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $DOM->AddAttribs('MenuLeft',Array('args'=>'Administrator/AddIns'));
 #-------------------------------------------------------------------------------
 $DOM->AddText('Title','Дополнения → Мастера настройки → Внешний вид');
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $Table = Array();
 #-------------------------------------------------------------------------------
@@ -29,59 +31,65 @@ $Table[] = 'Базовое оформление системы';
 #-------------------------------------------------------------------------------
 $Comp = Comp_Load('Upload','TopLogo');
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = Array('Верхний логотип (png)',$Comp);
 #-------------------------------------------------------------------------------
+$Table[] = new Tag('DIV',Array('style'=>'border:1px solid #DCDCDC;border:1px solid #DCDCDC;overflow:scroll;overflow-x:auto;overflow-y:auto;width:500px;height:150px;'),new Tag('IMG',Array('src'=>'SRC:{Images/TopLogo.png}')));
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 $Comp = Comp_Load('Upload','Favicon');
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
-#-------------------------------------------------------------------------------
-$Table[] = new Tag('DIV',Array('style'=>'border:1px solid #DCDCDC;border:1px solid #DCDCDC;overflow:scroll;overflow-x:auto;overflow-y:auto;width:500px;height:150px;'),new Tag('IMG',Array('src'=>'SRC:{Images/TopLogo.png}')));
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = Array('Иконка (ico)',$Comp);
 #-------------------------------------------------------------------------------
 $Table[] = new Tag('IMG',Array('src'=>'/favicon.ico'));
 #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$Comp = Comp_Load('Upload','FaviconPNG');
+if(Is_Error($Comp))
+	return ERROR | @Trigger_Error(500);
+#-------------------------------------------------------------------------------
+$Table[] = Array('Иконка для числа тикетов (png, 16x16)',$Comp);
+#-------------------------------------------------------------------------------
+$Table[] = new Tag('IMG',Array('src'=>'SRC:{Images/favicon.png}'));
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 $Copyright = DB_Select('Config','Value',Array('UNIQ','Where'=>"`Param` = 'Copyright'"));
 if(!Is_Array($Copyright))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
-$Comp = Comp_Load(
-  'Form/TextArea',
-  Array(
-    'name'  => 'Copyright',
-    'style' => 'width:100%;',
-    'rows'  => 4
-  ),
-  $Copyright['Value']
-);
+$Comp = Comp_Load('Form/TextArea',Array('name'=>'Copyright','style'=>'width:100%;','rows'=>4),$Copyright['Value']);
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = Array('Нижняя надпись',$Comp);
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $Table[] = 'Оформление счетов, формат *.bmp, без сжатия';
 #-------------------------------------------------------------------------------
 $Comp = Comp_Load('Upload','Logo');
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = Array('Логотип (300x150)',$Comp);
 #-------------------------------------------------------------------------------
 $Table[] = new Tag('DIV',Array('style'=>'border:1px solid #DCDCDC;overflow:scroll;overflow-x:auto;overflow-y:auto;width:300px;height:150px;'),new Tag('IMG',Array('src'=>'SRC:{Images/Logo.bmp}')));
 #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 $Comp = Comp_Load('Upload','Stamp');
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = Array('Печать (160x160)',$Comp);
 #-------------------------------------------------------------------------------
 $Table[] = new Tag('DIV',Array('style'=>'border:1px solid #DCDCDC;overflow:scroll;overflow-x:auto;overflow-y:auto;width:160px;height:160px;'),new Tag('IMG',Array('src'=>'SRC:{Images/Stamp.bmp}')));
 #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 $Comp = Comp_Load('Upload','dSign');
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = Array('Подпись директора (130x100)',$Comp);
 #-------------------------------------------------------------------------------
@@ -89,45 +97,42 @@ $Table[] = new Tag('DIV',Array('style'=>'border:1px solid #DCDCDC;overflow:scrol
 #-------------------------------------------------------------------------------
 $Comp = Comp_Load('Upload','aSign');
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = Array('Подпись главного бухгалтера (130x100)',$Comp);
 #-------------------------------------------------------------------------------
 $Table[] = new Tag('DIV',Array('style'=>'border:1px solid #DCDCDC;overflow:scroll;overflow-x:auto;overflow-y:auto;width:130px;height:100px;'),new Tag('IMG',Array('src'=>'SRC:{Images/aSign.bmp}')));
 #-------------------------------------------------------------------------------
-$Comp = Comp_Load(
-  'Form/Input',
-  Array(
-    'type'    => 'button',
-    'onclick' => "FormEdit('/Administrator/API/Appearanse','AppearanseForm','Сохранение внешнего вида');",
-    'value'   => 'Сохранить'
-  )
-);
+#-------------------------------------------------------------------------------
+$Comp = Comp_Load('Form/Input',Array('type'=>'button','onclick'=>"FormEdit('/Administrator/API/Appearanse','AppearanseForm','Сохранение внешнего вида');",'value'=>'Сохранить'));
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Table[] = $Comp;
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $Form = new Tag('FORM',Array('name'=>'AppearanseForm','onsubmit'=>'return false;'));
 #-------------------------------------------------------------------------------
 $Comp = Comp_Load('Tables/Standard',$Table);
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Form->AddChild($Comp);
 #-------------------------------------------------------------------------------
 $Comp = Comp_Load('Tab','Administrator/Masters',$Form);
 if(Is_Error($Comp))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $DOM->AddChild('Into',$Comp);
 #-------------------------------------------------------------------------------
 $Out = $DOM->Build();
 #-------------------------------------------------------------------------------
 if(Is_Error($Out))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 return $Out;
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
 ?>
