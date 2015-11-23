a8e0c7a4d0ee832515d1b97d7645cb9af909388d
Alex Keda <admin@lissyara.su>
2015-11-23 16:18:55 +0300

    JBS-1126: add settings, code clean

--- hosts/billing/system/classes/ICQ.class.php
+++ hosts/billing/system/classes/ICQ.class.php
@@ -7,71 +7,81 @@
  *
  */
 class ICQ implements Dispatcher {
-    /** Instance of email dispatcher. */
-    private static $instance;
-
-    /** Private. This dispatcher have only one instance. */
-    private function __construct() {
-
-    }
-
-    public static function get() {
-        if (!isset(self::$instance)) {
-            self::$instance = new ICQ();
-        }
-
-        return self::$instance;
-    }
-
-    public function send(Msg $msg) {
-        // Get template file path.
-        $templatePath = SPrintF('Notifies/ICQ/%s.tpl', $msg->getTemplate());
-
-        $smarty= JSmarty::get();
-
-        if (!$smarty->templateExists($templatePath)) {
-            throw new jException('Template file not found: '.$templatePath);
-        }
-
-        $smarty->assign('Config', Config());
-
-        foreach(array_keys($msg->getParams()) as $paramName) {
-            $smarty->assign($paramName, $msg->getParam($paramName));
-        }
-
-        try {
-            $message = $smarty->fetch($templatePath);
-        }
-        catch(Exception $e){
-            throw new jException(SPrintF("Can't fetch template: %s", $templatePath), $e->getCode(), $e);
-        }
-
-        $recipient = $msg->getParam('User');
-
-        if(!$recipient['Params']['NotificationMethods']['ICQ']['Address'])
-            throw new jException("ICQ UIN not found for user: ".$recipient['ID']);
-
-        $taskParams = Array(
-            'UserID' => $recipient['ID'],
-            'TypeID' => 'ICQ',
-            'Params' => Array(
-                $recipient['Params']['NotificationMethods']['ICQ']['Address'],
-                $message,
-                $recipient['ID']
-            )
-        );
-
-        $result = Comp_Load('www/Administrator/API/TaskEdit', $taskParams);
-        switch(ValueOf($result)) {
-            case 'error':
-              throw new jException("Couldn't add task to queue: ".$result);
-            case 'exception':
-              throw new jException("Couldn't add task to queue: ".$result->String);
-            case 'array':
-              return TRUE;
-            default:
-              throw new jException("Unexpected error.");
-        }
-    }
+	//------------------------------------------------------------------------------
+	/** Instance of email dispatcher. */
+	private static $instance;
+	//------------------------------------------------------------------------------
+	/** Private. This dispatcher have only one instance. */
+	private function __construct() {
+		//------------------------------------------------------------------------------
+		//------------------------------------------------------------------------------
+	}
+	//------------------------------------------------------------------------------
+	public static function get() {
+		//------------------------------------------------------------------------------
+		if (!isset(self::$instance))
+			self::$instance = new ICQ();
+		//------------------------------------------------------------------------------
+		return self::$instance;
+		//------------------------------------------------------------------------------
+	}
+	//------------------------------------------------------------------------------
+	public function send(Msg $msg) {
+		//------------------------------------------------------------------------------
+		// Get template file path.
+		$templatePath = SPrintF('Notifies/ICQ/%s.tpl', $msg->getTemplate());
+		//------------------------------------------------------------------------------
+		$smarty= JSmarty::get();
+		//------------------------------------------------------------------------------
+		if (!$smarty->templateExists($templatePath))
+			throw new jException('Template file not found: '.$templatePath);
+		//------------------------------------------------------------------------------
+		$smarty->assign('Config', Config());
+		//------------------------------------------------------------------------------
+		foreach(array_keys($msg->getParams()) as $paramName)
+			$smarty->assign($paramName, $msg->getParam($paramName));
+		//------------------------------------------------------------------------------
+		try {
+			//------------------------------------------------------------------------------
+			$message = $smarty->fetch($templatePath);
+			//------------------------------------------------------------------------------
+		}catch(Exception $e){
+			//------------------------------------------------------------------------------
+			throw new jException(SPrintF("Can't fetch template: %s", $templatePath), $e->getCode(), $e);
+			//------------------------------------------------------------------------------
+		}
+		//------------------------------------------------------------------------------
+		$recipient = $msg->getParam('User');
+		//------------------------------------------------------------------------------
+		if(!$recipient['Params']['NotificationMethods']['ICQ']['Address'])
+			throw new jException("ICQ UIN not found for user: ".$recipient['ID']);
+		//------------------------------------------------------------------------------
+		$taskParams = Array(
+					'UserID'	=> $recipient['ID'],
+					'TypeID'	=> 'ICQ',
+					'Params'	=> Array(
+								$recipient['Params']['NotificationMethods']['ICQ']['Address'],
+								$message,
+								$recipient['ID']
+								)
+					);
+		//------------------------------------------------------------------------------
+		$result = Comp_Load('www/Administrator/API/TaskEdit', $taskParams);
+		switch(ValueOf($result)){
+		case 'error':
+			throw new jException("Couldn't add task to queue: ".$result);
+		case 'exception':
+			throw new jException("Couldn't add task to queue: ".$result->String);
+		case 'array':
+			return TRUE;
+		default:
+			throw new jException("Unexpected error.");
+		}
+		//------------------------------------------------------------------------------
+	}
+	//------------------------------------------------------------------------------
 }
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
 ?>
