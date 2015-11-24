df960819d6a1e25b391326585195b4487a111334
Alex Keda <admin@lissyara.su>
2015-11-24 22:29:10 +0300

    JBS-1129: return previous version, if(!Function_Exists not work from web-interface

--- hosts/billing/comp/Tasks/RecoveryUsers.comp.php
+++ hosts/billing/comp/Tasks/RecoveryUsers.comp.php
@@ -76,67 +76,63 @@ if($UserID){
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-if(!Function_Exists('RepairUser')){
+function RepairUser($Users){
 	#-------------------------------------------------------------------------------
-	function RepairUser($Users){
+	$Template = System_XML('xml/Params/Users.xml');
+	if(Is_Error($Template))
+		return new gException('ERROR_TEMPLATE_LOAD','Ошибка загрузки шаблона');
+	#-------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------
+	foreach($Users as $User){
 		#-------------------------------------------------------------------------------
-		$Template = System_XML('xml/Params/Users.xml');
-		if(Is_Error($Template))
-			return new gException('ERROR_TEMPLATE_LOAD','Ошибка загрузки шаблона');
+		$Attribs = @$User['Params']['Settings'];
+		#-------------------------------------------------------------------------------
+		foreach(Array_Keys($Template['Settings']) as $AttribID)
+			if(!IsSet($Attribs[$AttribID]))
+				$Attribs[$AttribID] = $Template['Settings'][$AttribID]['Value'];
+		#-------------------------------------------------------------------------------
+		foreach(Array_Keys($Attribs) as $AttribID)
+			if(!IsSet($Template['Settings'][$AttribID]))
+				UnSet($Attribs[$AttribID]);
 		#-------------------------------------------------------------------------------
 		#-------------------------------------------------------------------------------
-		foreach($Users as $User){
+		$NotificationMethods = @$User['Params']['NotificationMethods'];
+		#-------------------------------------------------------------------------------
+		foreach(Array_Keys($Template['NotificationMethods']) as $Key){
 			#-------------------------------------------------------------------------------
-			$Attribs = @$User['Params']['Settings'];
+			if(!IsSet($NotificationMethods[$Key]))
+				$NotificationMethods[$Key] = Array();
 			#-------------------------------------------------------------------------------
-			foreach(Array_Keys($Template['Settings']) as $AttribID)
-				if(!IsSet($Attribs[$AttribID]))
-					$Attribs[$AttribID] = $Template['Settings'][$AttribID]['Value'];
-			#-------------------------------------------------------------------------------
-			foreach(Array_Keys($Attribs) as $AttribID)
-				if(!IsSet($Template['Settings'][$AttribID]))
-					UnSet($Attribs[$AttribID]);
-			#-------------------------------------------------------------------------------
-			#-------------------------------------------------------------------------------
-			$NotificationMethods = @$User['Params']['NotificationMethods'];
-			#-------------------------------------------------------------------------------
-			foreach(Array_Keys($Template['NotificationMethods']) as $Key){
-				#-------------------------------------------------------------------------------
-				if(!IsSet($NotificationMethods[$Key]))
-					$NotificationMethods[$Key] = Array();
-				#-------------------------------------------------------------------------------
-				foreach(Array_Keys($Template['NotificationMethods'][$Key]) as $Value)
-					if(!IsSet($NotificationMethods[$Key][$Value]))
-						$NotificationMethods[$Key][$Value] = $Template['NotificationMethods'][$Key][$Value];
-				#-------------------------------------------------------------------------------
-			}
-			#-------------------------------------------------------------------------------
-			foreach(Array_Keys($NotificationMethods) as $Key)
-				if(!IsSet($Template['NotificationMethods'][$Key]))
-					UnSet($NotificationMethods[$Key]);
-			#-------------------------------------------------------------------------------
-			#-------------------------------------------------------------------------------
-			$Params = Array();
-			#-------------------------------------------------------------------------------
-			$Params['Settings'] = $Attribs;
-			#-------------------------------------------------------------------------------
-			$Params['NotificationMethods'] = $NotificationMethods;
-			#-------------------------------------------------------------------------------
-			if(IsSet($User['Params']['IsAutoRegistered']) && $User['Params']['IsAutoRegistered']){
-				#-------------------------------------------------------------------------------
-				$Params['IsAutoRegistered'] = TRUE;
-				#-------------------------------------------------------------------------------
-			}else{
-				#-------------------------------------------------------------------------------
-				$Params['IsAutoRegistered'] = FALSE;
-				#-------------------------------------------------------------------------------
-			}
-			#-------------------------------------------------------------------------------
-			$IsUpdate = DB_Update('Users',Array('Email'=>StrToLower($User['Email']),'Params'=>$Params),Array('ID'=>$User['ID']));
-			if(Is_Error($IsUpdate))
-				return ERROR | @Trigger_Error(500);
+			foreach(Array_Keys($Template['NotificationMethods'][$Key]) as $Value)
+				if(!IsSet($NotificationMethods[$Key][$Value]))
+					$NotificationMethods[$Key][$Value] = $Template['NotificationMethods'][$Key][$Value];
 			#-------------------------------------------------------------------------------
 		}
+		#-------------------------------------------------------------------------------
+		foreach(Array_Keys($NotificationMethods) as $Key)
+			if(!IsSet($Template['NotificationMethods'][$Key]))
+				UnSet($NotificationMethods[$Key]);
+		#-------------------------------------------------------------------------------
+		#-------------------------------------------------------------------------------
+		$Params = Array();
+		#-------------------------------------------------------------------------------
+		$Params['Settings'] = $Attribs;
+		#-------------------------------------------------------------------------------
+		$Params['NotificationMethods'] = $NotificationMethods;
+		#-------------------------------------------------------------------------------
+		if(IsSet($User['Params']['IsAutoRegistered']) && $User['Params']['IsAutoRegistered']){
+			#-------------------------------------------------------------------------------
+			$Params['IsAutoRegistered'] = TRUE;
+			#-------------------------------------------------------------------------------
+		}else{
+			#-------------------------------------------------------------------------------
+			$Params['IsAutoRegistered'] = FALSE;
+			#-------------------------------------------------------------------------------
+		}
+		#-------------------------------------------------------------------------------
+		$IsUpdate = DB_Update('Users',Array('Email'=>StrToLower($User['Email']),'Params'=>$Params),Array('ID'=>$User['ID']));
+		if(Is_Error($IsUpdate))
+			return ERROR | @Trigger_Error(500);
 		#-------------------------------------------------------------------------------
 	}
 	#-------------------------------------------------------------------------------
