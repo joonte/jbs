d8aa5ef2222021d7d3d262b44a63baed24d6caa2
Alex Keda <admin@lissyara.su>
2016-06-30 11:56:40 +0300

    JBS-1192: code clean

--- hosts/hosting/comp/Tasks/HostingSchemeChange.comp.php
+++ hosts/hosting/comp/Tasks/HostingSchemeChange.comp.php
@@ -10,7 +10,7 @@ Eval(COMP_INIT);
 /******************************************************************************/
 /******************************************************************************/
 if(Is_Error(System_Load('classes/HostingServer.class.php')))
-  return ERROR | @Trigger_Error(500);
+	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $HostingOrder = DB_Select('HostingOrdersOwners',Array('ID','UserID','OrderID','SchemeID','(SELECT `ServerID` FROM `OrdersOwners` WHERE `OrdersOwners`.`ID` = `HostingOrdersOwners`.`OrderID`) AS `ServerID`','Login','(SELECT `Name` FROM `HostingSchemes` WHERE `HostingSchemes`.`ID` = `HostingOrdersOwners`.`OldSchemeID`) as `SchemeName`'),Array('UNIQ','ID'=>$HostingOrderID));
 #-------------------------------------------------------------------------------
@@ -20,165 +20,168 @@ case 'error':
 case 'exception':
 	return ERROR | @Trigger_Error(400);
 case 'array':
+	break;
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$HostingOrderID = (integer)$HostingOrder['ID'];
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# достаём историю статусов, с сортировкой в обратном направлении
+$Where = Array(
+		'`ModeID` = "HostingOrders"',
+		SPrintF('`RowID` = %u',$HostingOrderID)
+		);
+#-------------------------------------------------------------------------------
+$StatusesHistory = DB_Select('StatusesHistory','StatusID',Array('Where'=>$Where,'SortOn'=>'ID'));
+#-------------------------------------------------------------------------------
+switch(ValueOf($StatusesHistory)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	$StatusID = 'Active';
+	$Message  = 'История статусов не найдена, установлен статус по умолчанию';
+case 'array':
 	#-------------------------------------------------------------------------------
-	$HostingOrderID = (integer)$HostingOrder['ID'];
-	#-------------------------------------------------------------------------------
-	#-------------------------------------------------------------------------------
-	# достаём историю статусов, с сортировкой в обратном направлении
-	$Where = Array(
-			'`ModeID` = "HostingOrders"',
-			SPrintF('`RowID` = %u',$HostingOrderID)
-			);
-	#-------------------------------------------------------------------------------
-	$StatusesHistory = DB_Select('StatusesHistory','StatusID',Array('Where'=>$Where,'SortOn'=>'ID'));
-	#-------------------------------------------------------------------------------
-	switch(ValueOf($StatusesHistory)){
-	case 'error':
-		return ERROR | @Trigger_Error(500);
-	case 'exception':
-		$StatusID = 'Active';
-		$Message  = 'История статусов не найдена, установлен статус по умолчанию';
-	case 'array':
+	foreach($StatusesHistory as $Status){
 		#-------------------------------------------------------------------------------
-		foreach($StatusesHistory as $Status){
-			#-------------------------------------------------------------------------------
-			Debug(SPrintF('[comp/Tasks/HostingSchemeChange]: StatusID = %s',$Status['StatusID']));
-			#-------------------------------------------------------------------------------
-			if(In_Array($Status['StatusID'],Array('Active','Suspended')))
-				$StatusID = $Status['StatusID'];
-			#-------------------------------------------------------------------------------
-		}
+		Debug(SPrintF('[comp/Tasks/HostingSchemeChange]: StatusID = %s',$Status['StatusID']));
 		#-------------------------------------------------------------------------------
-		# не найден подходящий статус - ставим активный, и сообщение
-		if(!IsSet($StatusID)){
-			#-------------------------------------------------------------------------------
-			$StatusID = 'Active';
-			$Message  = 'Предыдущий статус не найден, установлен статус по умолчанию';
-			#-------------------------------------------------------------------------------
-		}
+		if(In_Array($Status['StatusID'],Array('Active','Suspended')))
+			$StatusID = $Status['StatusID'];
 		#-------------------------------------------------------------------------------
-		break;
-		#-------------------------------------------------------------------------------
-	default:
-		return ERROR | @Trigger_Error(101);
 	}
 	#-------------------------------------------------------------------------------
+	# не найден подходящий статус - ставим активный, и сообщение
+	if(!IsSet($StatusID)){
+		#-------------------------------------------------------------------------------
+		$StatusID = 'Active';
+		$Message  = 'Предыдущий статус не найден, установлен статус по умолчанию';
+		#-------------------------------------------------------------------------------
+	}
 	#-------------------------------------------------------------------------------
-	$HostingNewScheme = DB_Select('HostingSchemes','*',Array('UNIQ','ID'=>$HostingOrder['SchemeID']));
+	break;
 	#-------------------------------------------------------------------------------
-	switch(ValueOf($HostingNewScheme)){
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$HostingNewScheme = DB_Select('HostingSchemes','*',Array('UNIQ','ID'=>$HostingOrder['SchemeID']));
+#-------------------------------------------------------------------------------
+switch(ValueOf($HostingNewScheme)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	return ERROR | @Trigger_Error(400);
+case 'array':
+	break;
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$User = DB_Select('Users','*',Array('UNIQ','ID'=>$HostingOrder['UserID']));
+#-------------------------------------------------------------------------------
+switch(ValueOf($User)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+		return ERROR | @Trigger_Error(400);
+case 'array':
+	#-------------------------------------------------------------------------------
+	$HostingNewScheme['Email'] = $User['Email'];    # add email, for JBS-473
+	#-------------------------------------------------------------------------------
+	break;
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$ClassHostingServer = new HostingServer();
+#-------------------------------------------------------------------------------
+$IsSelected = $ClassHostingServer->Select((integer)$HostingOrder['ServerID']);
+#-------------------------------------------------------------------------------
+switch(ValueOf($IsSelected)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	return ERROR | @Trigger_Error(400);
+case 'true':
+	break;
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$GLOBALS['TaskReturnInfo'] = Array(($ClassHostingServer->Settings['Address'])=>Array($HostingOrder['Login']),($HostingOrder['SchemeName'])=>Array($HostingNewScheme['Name']));
+#-------------------------------------------------------------------------------
+#Debug(SPrintF("[comp/Tasks/HostingSchemeChange]: HostingNewScheme = %s",print_r($HostingNewScheme,true)));
+$SchemeChange = $ClassHostingServer->SchemeChange($HostingOrder['Login'],$HostingNewScheme);
+#-------------------------------------------------------------------------------
+switch(ValueOf($SchemeChange)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	#-------------------------------------------------------------------------------
+	$IsUpdate = DB_Update('HostingOrders',Array('SchemeID'=>$HostingSchemeID),Array('ID'=>$HostingOrderID));
+	if(Is_Error($IsUpdate))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'HostingOrders','StatusID'=>$StatusID,'RowsIDs'=>$HostingOrderID,'Comment'=>$SchemeChange->String));
+	#-------------------------------------------------------------------------------
+	switch(ValueOf($Comp)){
 	case 'error':
 		return ERROR | @Trigger_Error(500);
 	case 'exception':
 		return ERROR | @Trigger_Error(400);
 	case 'array':
-		#-------------------------------------------------------------------------------
-		$User = DB_Select('Users','*',Array('UNIQ','ID'=>$HostingOrder['UserID']));
-		#-------------------------------------------------------------------------------
-		switch(ValueOf($User)){
-		case 'error':
-			return ERROR | @Trigger_Error(500);
-		case 'exception':
-			return ERROR | @Trigger_Error(400);
-		case 'array':
-			#-------------------------------------------------------------------------------
-			$HostingNewScheme['Email'] = $User['Email'];	# add email, for JBS-473
-			#-------------------------------------------------------------------------------
-			break;
-			#-------------------------------------------------------------------------------
-		default:
-			return ERROR | @Trigger_Error(101);
-		}
-		#-------------------------------------------------------------------------------
-		#-------------------------------------------------------------------------------
-		$ClassHostingServer = new HostingServer();
-		#-------------------------------------------------------------------------------
-		$IsSelected = $ClassHostingServer->Select((integer)$HostingOrder['ServerID']);
-		#-------------------------------------------------------------------------------
-		switch(ValueOf($IsSelected)){
-		case 'error':
-			return ERROR | @Trigger_Error(500);
-		case 'exception':
-			return ERROR | @Trigger_Error(400);
-		case 'true':
-			#-------------------------------------------------------------------------------
-			$GLOBALS['TaskReturnInfo'] = Array(($ClassHostingServer->Settings['Address'])=>Array($HostingOrder['Login']),($HostingOrder['SchemeName'])=>Array($HostingNewScheme['Name']));
-			#-------------------------------------------------------------------------------
-			#Debug(SPrintF("[comp/Tasks/HostingSchemeChange]: HostingNewScheme = %s",print_r($HostingNewScheme,true)));
-			$SchemeChange = $ClassHostingServer->SchemeChange($HostingOrder['Login'],$HostingNewScheme);
-			#-------------------------------------------------------------------------------
-			switch(ValueOf($SchemeChange)){
-			case 'error':
-				return ERROR | @Trigger_Error(500);
-			case 'exception':
-				#-------------------------------------------------------------------------------
-				$IsUpdate = DB_Update('HostingOrders',Array('SchemeID'=>$HostingSchemeID),Array('ID'=>$HostingOrderID));
-				if(Is_Error($IsUpdate))
-					return ERROR | @Trigger_Error(500);
-				#-------------------------------------------------------------------------------
-				$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'HostingOrders','StatusID'=>$StatusID,'RowsIDs'=>$HostingOrderID,'Comment'=>$SchemeChange->String));
-				#-------------------------------------------------------------------------------
-				switch(ValueOf($Comp)){
-				case 'error':
-					return ERROR | @Trigger_Error(500);
-				case 'exception':
-					return ERROR | @Trigger_Error(400);
-				case 'array':
-					#-------------------------------------------------------------------------------
-					$Event = Array(
-							'UserID'	=> $HostingOrder['UserID'],
-							'PriorityID'	=> 'Error',
-							'Text'		=> SPrintF('Не удалось сменить тарифный план заказу хостинга [%s] в автоматическом режиме, причина (%s)',$HostingOrder['Login'],$SchemeChange->String),
-							'IsReaded'	=> FALSE
-							);
-					#-------------------------------------------------------------------------------
-					$Event = Comp_Load('Events/EventInsert',$Event);
-					if(!$Event)
-						return ERROR | @Trigger_Error(500);
-					#-------------------------------------------------------------------------------
-					return TRUE;
-					#-------------------------------------------------------------------------------
-				default:
-					return ERROR | @Trigger_Error(101);
-				}
-				#-------------------------------------------------------------------------------
-				case 'true':
-					#-------------------------------------------------------------------------------
-					$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'HostingOrders','StatusID'=>$StatusID,'RowsIDs'=>$HostingOrderID,'Comment'=>IsSet($Message)?$Message:'Тарифный план изменён'));
-					#-------------------------------------------------------------------------------
-					switch(ValueOf($Comp)){
-					case 'error':
-						return ERROR | @Trigger_Error(500);
-					case 'exception':
-						return ERROR | @Trigger_Error(400);
-					case 'array':
-						#-------------------------------------------------------------------------------
-					$Event = Array(
-							'UserID'	=> $HostingOrder['UserID'],
-							'PriorityID'	=> 'Hosting',
-							'Text'		=> SPrintF('Успешно изменён тарифный план (%s->%s) заказа на хостинг [%s], сервер (%s)',$HostingOrder['SchemeName'],$HostingNewScheme['Name'],$HostingOrder['Login'],$ClassHostingServer->Settings['Address']),
-							);
-					$Event = Comp_Load('Events/EventInsert',$Event);
-					if(!$Event)
-						return ERROR | @Trigger_Error(500);
-					#-------------------------------------------------------------------------------
-					return TRUE;
-					#-------------------------------------------------------------------------------
-				default:
-					return ERROR | @Trigger_Error(101);
-				}
-				#-------------------------------------------------------------------------------
-			default:
-				return ERROR | @Trigger_Error(101);
-			}
-			#-------------------------------------------------------------------------------
-		default:
-			return ERROR | @Trigger_Error(101);
-		}
-		#-------------------------------------------------------------------------------
+		break;
 	default:
 		return ERROR | @Trigger_Error(101);
 	}
+	#-------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------
+	$Event = Array(
+			'UserID'	=> $HostingOrder['UserID'],
+			'PriorityID'	=> 'Error',
+			'Text'		=> SPrintF('Не удалось сменить тарифный план заказу хостинга [%s] в автоматическом режиме, причина (%s)',$HostingOrder['Login'],$SchemeChange->String),
+			'IsReaded'	=> FALSE
+			);
+	#-------------------------------------------------------------------------------
+	$Event = Comp_Load('Events/EventInsert',$Event);
+	if(!$Event)
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	return TRUE;
+	#-------------------------------------------------------------------------------
+case 'true':
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'HostingOrders','StatusID'=>$StatusID,'RowsIDs'=>$HostingOrderID,'Comment'=>IsSet($Message)?$Message:'Тарифный план изменён'));
+	#-------------------------------------------------------------------------------
+	switch(ValueOf($Comp)){
+	case 'error':
+		return ERROR | @Trigger_Error(500);
+	case 'exception':
+		return ERROR | @Trigger_Error(400);
+	case 'array':
+		break;
+	default:
+		return ERROR | @Trigger_Error(101);
+	}
+	#-------------------------------------------------------------------------------
+	$Event = Array(
+			'UserID'	=> $HostingOrder['UserID'],
+			'PriorityID'	=> 'Hosting',
+			'Text'		=> SPrintF('Успешно изменён тарифный план (%s->%s) заказа на хостинг [%s], сервер (%s)',$HostingOrder['SchemeName'],$HostingNewScheme['Name'],$HostingOrder['Login'],$ClassHostingServer->Settings['Address']),
+			);
+	$Event = Comp_Load('Events/EventInsert',$Event);
+	if(!$Event)
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	return TRUE;
 	#-------------------------------------------------------------------------------
 default:
 	return ERROR | @Trigger_Error(101);
