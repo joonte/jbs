b15a9ff9914765394e8b6e8cddd901ac937d2868
Alex Keda <lissyara@dacha.lissyara.su>
2016-08-12 21:16:49 +0300

    JBS-231: code clean

--- hosts/hosting/comp/Tasks/ISPswForDelete.comp.php
+++ hosts/hosting/comp/Tasks/ISPswForDelete.comp.php
@@ -8,47 +8,54 @@ Eval(COMP_INIT);
 /******************************************************************************/
 /******************************************************************************/
 $Config = Config();
+#-------------------------------------------------------------------------------
 $DeleteTimeout = $Config['Tasks']['Types']['ISPswForDelete']['DeleteTimeout'] * 24 * 3600;
+#-------------------------------------------------------------------------------
 $Where = "`StatusID` = 'Suspended' AND `StatusDate` + $DeleteTimeout - UNIX_TIMESTAMP() <= 0";
 #-------------------------------------------------------------------------------
 $ISPswOrders = DB_Select('ISPswOrders','ID',Array('Where'=>$Where));
 #-------------------------------------------------------------------------------
 switch(ValueOf($ISPswOrders)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    # No more...
-  break;
-  case 'array':
-    #---------------------------------------------------------------------------
-    $GLOBALS['TaskReturnInfo'] = SPrintF('Deleted %s accounts',SizeOf($ISPswOrders));
-    #---------------------------------------------------------------------------
-    foreach($ISPswOrders as $ISPswOrder){
-      #-------------------------------------------------------------------------
-      $Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'ISPswOrders','StatusID'=>'Deleted','RowsIDs'=>$ISPswOrder['ID'],'Comment'=>'Срок блокировки заказа окончен'));
-      #-------------------------------------------------------------------------
-      switch(ValueOf($Comp)){
-        case 'error':
-          return ERROR | @Trigger_Error(500);
-        case 'exception':
-          return ERROR | @Trigger_Error(400);
-        case 'array':
-          # No more...
-        break;
-        default:
-          return ERROR | @Trigger_Error(101);
-      }
-    }
-  break;
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	# No more...
+	break;
+case 'array':
+	#-------------------------------------------------------------------------------
+	$GLOBALS['TaskReturnInfo'] = SPrintF('Deleted %s accounts',SizeOf($ISPswOrders));
+	#-------------------------------------------------------------------------------
+	foreach($ISPswOrders as $ISPswOrder){
+		#-------------------------------------------------------------------------------
+		$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'ISPswOrders','StatusID'=>'Deleted','RowsIDs'=>$ISPswOrder['ID'],'Comment'=>'Срок блокировки заказа окончен'));
+		#-------------------------------------------------------------------------------
+		switch(ValueOf($Comp)){
+		case 'error':
+			return ERROR | @Trigger_Error(500);
+		case 'exception':
+			return ERROR | @Trigger_Error(400);
+		case 'array':
+			# No more...
+			break;
+		default:
+			return ERROR | @Trigger_Error(101);
+		}
+		#-------------------------------------------------------------------------------
+	}
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $StartHour    = $Config['Tasks']['Types']['ISPswForDelete']['StartHour'];
 $StartMinutes = $Config['Tasks']['Types']['ISPswForDelete']['StartMinutes'];
 #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 return MkTime($StartHour,$StartMinutes,0,Date('n'),Date('j')+1,Date('Y'));
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
 ?>
