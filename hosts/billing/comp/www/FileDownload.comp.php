568a84b734913f6a752726b6fb0a6acaa5386494
Alex Keda <admin@lissyara.su>
2017-06-05 10:57:38 +0300

    JBS-659: code clean

--- hosts/billing/comp/www/FileDownload.comp.php
+++ hosts/billing/comp/www/FileDownload.comp.php
@@ -17,51 +17,53 @@ if(Is_Error(System_Load('modules/Authorisation.mod','libs/HTMLDoc.php','libs/Upl
 #-------------------------------------------------------------------------------
 $TypeID = DB_Escape($TypeID);
 #-------------------------------------------------------------------------------
-$FileData = DB_Select($TypeID,'*',Array('UNIQ','ID'=>$FileID));
+$FileData = DB_Select(SPrintF('%sOwners',$TypeID),'*',Array('UNIQ','ID'=>$FileID));
 #-------------------------------------------------------------------------------
 switch(ValueOf($FileData)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    return ERROR | @Trigger_Error(400);
-  case 'array':
-    #---------------------------------------------------------------------------
-    $Permission = Permission_Check('EdeskRead',(integer)$GLOBALS['__USER']['ID'],(integer)$FileData['UserID']);
-    #---------------------------------------------------------------------------
-    switch(ValueOf($Permission)){
-      case 'error':
-        return ERROR | @Trigger_Error(500);
-      case 'exception':
-        return ERROR | @Trigger_Error(400);
-      case 'false':
-        return ERROR | @Trigger_Error(700);
-      case 'true':
-        #-----------------------------------------------------------------------
-        $Length = GetUploadedFileSize($TypeID, $FileID);
-        #-----------------------------------------------------------------------
-        if(!$Length)
-          return new gException('CANNOT_GET_FILE_SIZE','Не удалось получить размер файла');
-        #-----------------------------------------------------------------------
-        $Data = GetUploadedFile($TypeID, $FileID);
-	#-----------------------------------------------------------------------
-	$FileName = SPrintF('%s.bin',$FileData['ID']);
-	if($TypeID == 'EdesksMessages')	{$FileName = $FileData['FileName'];}
-	if($TypeID == 'Profiles')	{$FileName = SPrintF('document_%s.%s',$FileData['ID'],$FileData['Format']);}
-        #-------------------------------------------------------------------------------
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	return ERROR | @Trigger_Error(400);
+case 'array':
 	#-------------------------------------------------------------------------------
-        Header(SPrintF('Content-Type: %s; charset=utf-8',GetFileMimeType($TypeID,$FileID)));
-        Header(SPrintF('Content-Length: %u',$Length));
-        Header(SPrintF('Content-Disposition: attachment; filename="%s";',$FileName));
-        Header('Pragma: nocache');
-        #-----------------------------------------------------------------------
-        return $Data['Data'];
-      default:
-        return ERROR | @Trigger_Error(101);
-    }
-  default:
-    return ERROR | @Trigger_Error(101);
+	$Permission = Permission_Check('EdeskRead',(integer)$GLOBALS['__USER']['ID'],(integer)$FileData['UserID']);
+	#-------------------------------------------------------------------------------
+	switch(ValueOf($Permission)){
+	case 'error':
+		return ERROR | @Trigger_Error(500);
+	case 'exception':
+		return ERROR | @Trigger_Error(400);
+	case 'false':
+		return ERROR | @Trigger_Error(700);
+	case 'true':
+		#-------------------------------------------------------------------------------
+		$Length = GetUploadedFileSize($TypeID, $FileID);
+		#-------------------------------------------------------------------------------
+		if(!$Length)
+			return new gException('CANNOT_GET_FILE_SIZE','Не удалось получить размер файла');
+		#-------------------------------------------------------------------------------
+		$Data = GetUploadedFile($TypeID, $FileID);
+		#-------------------------------------------------------------------------------
+		$FileName = SPrintF('%s.bin',$FileData['ID']);
+		if($TypeID == 'Profiles')	{$FileName = SPrintF('document_%s.%s',$FileData['ID'],$FileData['Format']);}
+		if(IsSet($FileData['FileName'])){$FileName = $FileData['FileName'];}
+		#-------------------------------------------------------------------------------
+		#-------------------------------------------------------------------------------
+		Header(SPrintF('Content-Type: %s; charset=utf-8',GetFileMimeType($TypeID,$FileID)));
+		Header(SPrintF('Content-Length: %u',$Length));
+		Header(SPrintF('Content-Disposition: attachment; filename="%s";',$FileName));
+		Header('Pragma: nocache');
+		#-------------------------------------------------------------------------------
+		return $Data['Data'];
+		#-------------------------------------------------------------------------------
+	default:
+		return ERROR | @Trigger_Error(101);
+	}
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
 #-------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------
 
 ?>
