713a6194f1687f6b155b3702dc7fd3949190fc8a
Alex Keda <admin@lissyara.su>
2017-11-18 16:45:57 +0300

    JBS-1234: set int for memory/swap columns

---
+++ patches/hosting/db/1000319.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `VPSSchemes` CHANGE `mem` `mem` INT(7) DEFAULT '0';
+-- SEPARATOR
+ALTER TABLE `VPSSchemes` CHANGE `bmem` `bmem` INT(7) DEFAULT '0';
+-- SEPARATOR
+ALTER TABLE `VPSSchemes` CHANGE `maxswap` `maxswap` INT(7) DEFAULT '1';
+
