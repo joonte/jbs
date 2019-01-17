d75f163c37423fcb8ab1a988d00250e700766296
Alex Keda <lissyara@HP.lissyara.su>
2019-01-17 20:44:55 +0300

    JBS-1063: fix default value

--- patches/hosting/db/1000324.sql
+++ patches/hosting/db/1000324.sql
@@ -1,3 +1,8 @@
 
 ALTER TABLE `Orders` CHANGE `DaysRemainded` `DaysRemainded` INT(11) DEFAULT '0';
+-- SEPARATOR
+ALTER TABLE `Orders` CHANGE `AdminNotice` `AdminNotice` TEXT;
+-- SEPARATOR
+ALTER TABLE `Orders` CHANGE `UserNotice` `UserNotice` TEXT;
+
 
