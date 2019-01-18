6e4e2df6ce9f52596b21e35735363a3527a9a775
Alex Keda <admin@lissyara.su>
2019-01-18 09:12:00 +0300

    JBS-1063: fix default value

--- patches/hosting/db/1000324.sql
+++ patches/hosting/db/1000324.sql
@@ -4,5 +4,10 @@ ALTER TABLE `Orders` CHANGE `DaysRemainded` `DaysRemainded` INT(11) DEFAULT '0';
 ALTER TABLE `Orders` CHANGE `AdminNotice` `AdminNotice` TEXT;
 -- SEPARATOR
 ALTER TABLE `Orders` CHANGE `UserNotice` `UserNotice` TEXT;
-
+-- SEPARATOR
+ALTER TABLE `Edesks` CHANGE `SeenByPersonal` `SeenByPersonal` INT(11) DEFAULT '0';
+-- SEPARATOR
+ALTER TABLE `Edesks` CHANGE `LastSeenBy` `LastSeenBy` INT(11) DEFAULT '0';
+-- SEPARATOR
+ALTER TABLE `Edesks` CHANGE `SeenByUser` `SeenByUser` INT(11) DEFAULT '0';
 
