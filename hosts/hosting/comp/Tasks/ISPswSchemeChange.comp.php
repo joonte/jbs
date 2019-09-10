baa35248ee9d9e3b8cf1da6d4616286b76092390
Alex Keda <lissyara@HP.lissyara.su>
2019-09-10 21:56:47 +0300

    JBS-1270: allow scheme change on locked orders, ISPsw

--- hosts/hosting/comp/Tasks/ISPswSchemeChange.comp.php
+++ hosts/hosting/comp/Tasks/ISPswSchemeChange.comp.php
@@ -21,100 +21,148 @@ case 'error':
 case 'exception':
 	return ERROR | @Trigger_Error(400);
 case 'array':
+	break;
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# достаём историю статусов, с сортировкой в обратном направлении
+$Where = Array(
+		'`ModeID` = "ISPswOrders"',
+		SPrintF('`RowID` = %u',$ISPswOrderID)
+		);
+#-------------------------------------------------------------------------------
+$StatusesHistory = DB_Select('StatusesHistory','StatusID',Array('Where'=>$Where,'SortOn'=>'ID'));
+#-------------------------------------------------------------------------------
+switch(ValueOf($StatusesHistory)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
 	#-------------------------------------------------------------------------------
-        $Server = DB_Select('Servers','*',Array('UNIQ','ID'=>$ISPswOrder['ServerID']));
+	$StatusID = 'Active';
 	#-------------------------------------------------------------------------------
-	switch(ValueOf($Server)){
+	$Message  = 'История статусов не найдена, установлен статус по умолчанию';
+	#-------------------------------------------------------------------------------
+case 'array':
+	#-------------------------------------------------------------------------------
+	foreach($StatusesHistory as $Status){
+		#-------------------------------------------------------------------------------
+		Debug(SPrintF('[comp/Tasks/ISPswSchemeChange]: StatusID = %s',$Status['StatusID']));
+		#-------------------------------------------------------------------------------
+		if(In_Array($Status['StatusID'],Array('Active','Suspended')))
+			$StatusID = $Status['StatusID'];
+		#-------------------------------------------------------------------------------
+	}
+	#-------------------------------------------------------------------------------
+	# не найден подходящий статус - ставим активный, и сообщение
+	if(!IsSet($StatusID)){
+		#-------------------------------------------------------------------------------
+		$StatusID = 'Active';
+		$Message  = 'Предыдущий статус не найден, установлен статус по умолчанию';
+		#-------------------------------------------------------------------------------
+	}
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
+
+}
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$Server = DB_Select('Servers','*',Array('UNIQ','ID'=>$ISPswOrder['ServerID']));
+#-------------------------------------------------------------------------------
+switch(ValueOf($Server)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	return ERROR | @Trigger_Error(400);
+case 'array':
+	#-------------------------------------------------------------------------------
+	Debug(SPrintF('[comp/Tasks/ISPswSchemeChange]: found server: Address = %s; ID = %s',$Server['Address'],$Server['ID']));
+	#-------------------------------------------------------------------------------
+	break;
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+$ISPswOrderID = (integer)$ISPswOrder['ID'];
+#-------------------------------------------------------------------------------
+$ISPswNewScheme = DB_Select('ISPswSchemes','*',Array('UNIQ','ID'=>$ISPswOrder['SchemeID']));
+#-------------------------------------------------------------------------------
+switch(ValueOf($ISPswNewScheme)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	return ERROR | @Trigger_Error(400);
+case 'array':
+	break;
+default:
+	return ERROR | @Trigger_Error(101);
+}
+#-------------------------------------------------------------------------------
+$SchemeChange = BillManager_Scheme_Change($Server,$ISPswNewScheme);
+#-------------------------------------------------------------------------------
+switch(ValueOf($SchemeChange)){
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	#-------------------------------------------------------------------------------
+	$IsUpdate = DB_Update('ISPswOrders',Array('SchemeID'=>$ISPswSchemeID),Array('ID'=>$ISPswOrderID));
+	if(Is_Error($IsUpdate))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'ISPswOrders','StatusID'=>$StatusID,'RowsIDs'=>$ISPswOrderID,'Comment'=>$SchemeChange->String));
+	#-------------------------------------------------------------------------------
+	switch(ValueOf($Comp)){
 	case 'error':
 		return ERROR | @Trigger_Error(500);
 	case 'exception':
 		return ERROR | @Trigger_Error(400);
 	case 'array':
-		Debug(SPrintF('[comp/Tasks/ISPswSchemeChange]: found server: Address = %s; ID = %s',$Server['Address'],$Server['ID']));
-		break;
+		#-------------------------------------------------------------------------------
+		$Event = Array(
+				'UserID'	=> $ISPswOrder['UserID'],
+				'PriorityID'	=> 'Error',
+				'Text'	=> SPrintF('Не удалось сменить тарифный план заказу ПО ISPsystem (%s) в автоматическом режиме, причина (%s)',$ISPswOrder['IP'],$SchemeChange->String),
+				'IsReaded'	=> FALSE
+				);
+		$Event = Comp_Load('Events/EventInsert',$Event);
+		if(!$Event)
+			return ERROR | @Trigger_Error(500);
+		#-------------------------------------------------------------------------------
+		return TRUE;
+		#-------------------------------------------------------------------------------
 	default:
 		return ERROR | @Trigger_Error(101);
 	}
 	#-------------------------------------------------------------------------------
+case 'true':
 	#-------------------------------------------------------------------------------
-	$ISPswOrderID = (integer)$ISPswOrder['ID'];
+	$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'ISPswOrders','StatusID'=>$StatusID,'RowsIDs'=>$ISPswOrderID,'Comment'=>'Тарифный план изменен'));
 	#-------------------------------------------------------------------------------
-	$ISPswNewScheme = DB_Select('ISPswSchemes','*',Array('UNIQ','ID'=>$ISPswOrder['SchemeID']));
-	#-------------------------------------------------------------------------------
-	switch(ValueOf($ISPswNewScheme)){
+	switch(ValueOf($Comp)){
 	case 'error':
 		return ERROR | @Trigger_Error(500);
 	case 'exception':
 		return ERROR | @Trigger_Error(400);
 	case 'array':
 		#-------------------------------------------------------------------------------
-		$SchemeChange = BillManager_Scheme_Change($Server,$ISPswNewScheme);
-		#-------------------------------------------------------------------------------
-		switch(ValueOf($SchemeChange)){
-		case 'error':
+		$Event = Array(
+				'UserID'	=> $ISPswOrder['UserID'],
+				'PriorityID'	=> 'Hosting',
+				'Text'	=> SPrintF('Тарифный план заказа ПО ISPsystem (%s) изменён на (%s)',$ISPswOrder['IP'],$ISPswNewScheme['Name']),
+				);
+		$Event = Comp_Load('Events/EventInsert',$Event);
+		if(!$Event)
 			return ERROR | @Trigger_Error(500);
-		case 'exception':
-			#-------------------------------------------------------------------------------
-			$IsUpdate = DB_Update('ISPswOrders',Array('SchemeID'=>$ISPswSchemeID),Array('ID'=>$ISPswOrderID));
-			if(Is_Error($IsUpdate))
-				return ERROR | @Trigger_Error(500);
-			#-------------------------------------------------------------------------------
-			$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'ISPswOrders','StatusID'=>'Active','RowsIDs'=>$ISPswOrderID,'Comment'=>$SchemeChange->String));
-			#-------------------------------------------------------------------------------
-			switch(ValueOf($Comp)){
-			case 'error':
-				return ERROR | @Trigger_Error(500);
-			case 'exception':
-				return ERROR | @Trigger_Error(400);
-			case 'array':
-				#-------------------------------------------------------------------------------
-				$Event = Array(
-						'UserID'	=> $ISPswOrder['UserID'],
-						'PriorityID'	=> 'Error',
-						'Text'	=> SPrintF('Не удалось сменить тарифный план заказу ПО ISPsystem (%s) в автоматическом режиме, причина (%s)',$ISPswOrder['IP'],$SchemeChange->String),
-						'IsReaded'	=> FALSE
-						);
-				$Event = Comp_Load('Events/EventInsert',$Event);
-				if(!$Event)
-					return ERROR | @Trigger_Error(500);
-				#-------------------------------------------------------------------------------
-				return TRUE;
-				#-------------------------------------------------------------------------------
-			default:
-				return ERROR | @Trigger_Error(101);
-			}
-			#-------------------------------------------------------------------------------
-		case 'true':
-			#-------------------------------------------------------------------------------
-			$Comp = Comp_Load('www/API/StatusSet',Array('ModeID'=>'ISPswOrders','StatusID'=>'Active','RowsIDs'=>$ISPswOrderID,'Comment'=>'Тарифный план изменен'));
-			#-------------------------------------------------------------------------------
-			switch(ValueOf($Comp)){
-			case 'error':
-				return ERROR | @Trigger_Error(500);
-			case 'exception':
-				return ERROR | @Trigger_Error(400);
-			case 'array':
-				#-------------------------------------------------------------------------------
-				$Event = Array(
-						'UserID'	=> $ISPswOrder['UserID'],
-						'PriorityID'	=> 'Hosting',
-						'Text'	=> SPrintF('Тарифный план заказа ПО ISPsystem (%s) изменён на (%s)',$ISPswOrder['IP'],$ISPswNewScheme['Name']),
-						);
-				$Event = Comp_Load('Events/EventInsert',$Event);
-				if(!$Event)
-					return ERROR | @Trigger_Error(500);
-				#-------------------------------------------------------------------------------
-				$GLOBALS['TaskReturnInfo'] = Array($ISPswOrder['IP'],$ISPswNewScheme['Name']);
-				#-------------------------------------------------------------------------------
-				return TRUE;
-				#-------------------------------------------------------------------------------
-			default:
-				return ERROR | @Trigger_Error(101);
-			}
-			#-------------------------------------------------------------------------------
-		default:
-			return ERROR | @Trigger_Error(101);
-		}
+		#-------------------------------------------------------------------------------
+		$GLOBALS['TaskReturnInfo'] = Array($ISPswOrder['IP'],$ISPswNewScheme['Name']);
+		#-------------------------------------------------------------------------------
+		return TRUE;
 		#-------------------------------------------------------------------------------
 	default:
 		return ERROR | @Trigger_Error(101);
@@ -124,5 +172,5 @@ default:
 	return ERROR | @Trigger_Error(101);
 }
 #-------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------
 ?>
