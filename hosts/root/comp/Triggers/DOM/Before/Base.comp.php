27d81a06741080d5fa3df19695e93f968469a309
Alex Keda <admin@lissyara.su>
2019-10-07 11:47:05 +0300

    JBS-618: set maximum-scale=1.0, minimum-scale=1.0, user-scalable=no

--- hosts/root/comp/Triggers/DOM/Before/Base.comp.php
+++ hosts/root/comp/Triggers/DOM/Before/Base.comp.php
@@ -50,7 +50,8 @@ $DOM->AddChild('Head',new Tag('LINK',Array('rel'=>'stylesheet','type'=>'text/css
 $DOM->AddChild('Head',new Tag('LINK',Array('rel'=>'stylesheet','href'=>'SRC:{Css/Mobile.css}','media'=>'only screen and (max-width:600px)')));
 #-------------------------------------------------------------------------------
 // для мобильных устройств, чтоб не умничали и присылали реально разрешение браузера
-$DOM->AddChild('Head',new Tag('META',Array('name'=>'viewport','content'=>'width=device-width; initial-scale=1')));
+//$DOM->AddChild('Head',new Tag('META',Array('name'=>'viewport','content'=>'width=device-width, initial-scale=1, shrink-to-fit=no')));
+$DOM->AddChild('Head',new Tag('META',Array('name'=>'viewport','content'=>'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, shrink-to-fit=no')));
 #-------------------------------------------------------------------------------
 $DOM->AddHTML('Floating',TemplateReplace('Triggers.DOM.Before.Base.TABLE'));
 #-------------------------------------------------------------------------------
