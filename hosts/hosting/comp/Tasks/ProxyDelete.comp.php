a7ecbd714e18ada117c8fb69e838baa29995987e
Alex Keda <lissyara@HP.lissyara.su>
2020-02-24 21:04:36 +0300

    JBS-1349: fix delete error

--- hosts/hosting/comp/Tasks/ProxyDelete.comp.php
+++ hosts/hosting/comp/Tasks/ProxyDelete.comp.php
@@ -13,7 +13,7 @@ if(Is_Error(System_Load('classes/ProxyServer.class.php')))
 	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 $Columns = Array(
-		'ID','UserID','OrderID',
+		'ID','UserID','OrderID','Host','Port',
 		'(SELECT `ServerID` FROM `OrdersOwners` WHERE `OrdersOwners`.`ID` = `ProxyOrdersOwners`.`OrderID`) AS `ServerID`',
 		'(SELECT `Name` FROM `ProxySchemes` WHERE `ProxySchemes`.`ID` = `ProxyOrdersOwners`.`SchemeID`) as `SchemeName`'
 		);
@@ -49,15 +49,17 @@ case 'array':
 			$Event = Array(
 					'UserID'	=> $ProxyOrder['UserID'],
 					'PriorityID'	=> 'Hosting',
-					'Text'		=> SPrintF('Заказ на прокси-сервер [%s], тариф (%s) удален',$ProxyOrderID,$ProxyOrder['SchemeName'])
+					'Text'		=> SPrintF('Заказ на прокси-сервер [%s], тариф (%s) удален',$ProxyOrder['OrderID'],$ProxyOrder['SchemeName'])
 					);
 			$Event = Comp_Load('Events/EventInsert',$Event);
 			if(!$Event)
 				return ERROR | @Trigger_Error(500);
 			#-------------------------------------------------------------------------------
-			$GLOBALS['TaskReturnInfo'] = Array(($ClassProxyServer->Settings['Address'])=>Array($ProxyOrder['Login'],$ProxyOrder['SchemeName']));
+			$GLOBALS['TaskReturnInfo'] = Array(($ClassProxyServer->Settings['Address'])=>Array(SPrintF('%s:%u',$ProxyOrder['Host'],$ProxyOrder['Port']),$ProxyOrder['SchemeName']));
+			#-------------------------------------------------------------------------------
 			#-------------------------------------------------------------------------------
 			return TRUE;
+			#-------------------------------------------------------------------------------
 			#-------------------------------------------------------------------------------
 		default:
 			return ERROR | @Trigger_Error(101);
