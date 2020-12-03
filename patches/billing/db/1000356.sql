56700e70196217a1f07ddafcd6093447027ea84b
Alex Keda <lissyara@HP.lissyara.su>
2020-12-03 09:32:58 +0300

    JBS-1480: ICQ давно умерло, по крайней мере API

---
+++ patches/billing/db/1000356.sql
@@ -0,0 +1,11 @@
+
+DELETE FROM `Servers` WHERE `TemplateID` = 'ICQ';
+
+-- SEPARATOR
+
+DELETE FROM `Tasks` WHERE `TypeID` = 'ICQ';
+
+-- SEPARATOR
+
+DELETE FROM `Contacts` WHERE `MethodID` = 'ICQ';
+
