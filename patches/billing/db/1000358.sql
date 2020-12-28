5fd3eb27620339ee7e130cadfbd74d8665b5682c
Alex Keda <lissyara@HP.lissyara.su>
2020-12-28 22:00:04 +0300

    JBS-1504: яндекс деньги в юкасса, p2p пока оставил как есть, тестить не на чем

--- patches/billing/db/1000358.sql
+++ patches/billing/db/1000358.sql
@@ -1,3 +1,11 @@
 
 UPDATE `Invoices` SET `PaymentSystemID` = 'YooMoney' WHERE `PaymentSystemID` = 'Yandex';
 
+-- SEPARATOR
+
+UPDATE `Clauses` SET `Partition` = 'Invoices/PaymentSystems/YooMoney' WHERE `Partition` = 'Invoices/PaymentSystems/Yandex';
+
+-- SEPARATOR
+
+INSERT INTO `PaymentSystemsCollation` VALUES (NULL,'yes',30,'YooMoney','ЮMoney','YooMoney.png','ЮMoney (ранее: Яндекс.Деньги), это платежная система, которая позволяет оплачивать различные товары и услуги; совершать безопасные платежи в интернете; надежно хранить всю информацию о ваших зачислениях и платежах.','ЮMoney','');
+
