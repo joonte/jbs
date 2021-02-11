68f7b300615d830e7fb8c9e7149e9191e31df5bf
Alex Keda <lissyara@HP.lissyara.su>
2021-02-11 22:02:17 +0300

    JBS-1534: заменил company.com на example.com

--- patches/billing/db/1000184.sql
+++ patches/billing/db/1000184.sql
@@ -1,4 +1,4 @@
 INSERT INTO `Users`
   (`ID`,`GroupID`,`OwnerID`,`Name`,`Watchword`,`Email`,`Sign`,`IsActive`,`IsProtected`)
 VALUES
-(200,3000000,1,'Клиент',MD5('nopassword'),'client@company.com','С уважением, Клиент.','no','yes');
+(200,3000000,1,'Клиент',MD5('nopassword'),'client@example.com','С уважением, Клиент.','no','yes');
