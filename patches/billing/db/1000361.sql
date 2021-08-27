42ea5281ee58461a6e457abdd6917171421b82d6
Alex Keda <admin@lissyara.su>
2021-08-27 11:45:13 +0300

    JBS-1558: увеличил поля под IP адреса и имена

---
+++ patches/billing/db/1000361.sql
@@ -0,0 +1,19 @@
+
+ALTER TABLE `DomainOrders` CHANGE `Ns1Name` `Ns1Name` char(64) default '';
+-- SEPARATOR
+ALTER TABLE `DomainOrders` CHANGE `Ns1IP` `Ns1IP` char(64) default '';
+-- SEPARATOR
+ALTER TABLE `DomainOrders` CHANGE `Ns2Name` `Ns2Name` char(64) default '';
+-- SEPARATOR
+ALTER TABLE `DomainOrders` CHANGE `Ns2IP` `Ns2IP` char(64) default '';
+-- SEPARATOR
+ALTER TABLE `DomainOrders` CHANGE `Ns3Name` `Ns3Name` char(64) default '';
+-- SEPARATOR
+ALTER TABLE `DomainOrders` CHANGE `Ns3IP` `Ns3IP` char(64) default '';
+-- SEPARATOR
+ALTER TABLE `DomainOrders` CHANGE `Ns4Name` `Ns4Name` char(64) default '';
+-- SEPARATOR
+ALTER TABLE `DomainOrders` CHANGE `Ns4IP` `Ns4IP` char(64) default '';
+-- SEPARATOR
+
+
