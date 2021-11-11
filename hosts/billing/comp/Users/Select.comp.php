08760f44be5051093d64a5de581378a016b93de9
Alex Keda <admin@lissyara.su>
2021-11-11 09:44:39 +0300

    JBS-1584: удалил старый jquery, переместил картинки, поправил ошибку селектора юзера

--- hosts/billing/comp/Users/Select.comp.php
+++ hosts/billing/comp/Users/Select.comp.php
@@ -1,8 +1,7 @@
 <?php
 
-
 #-------------------------------------------------------------------------------
-/** @author Великодный В.В. (Joonte Ltd.) */
+/** @author Alex Keda, for www.host-food.ru */
 /******************************************************************************/
 /******************************************************************************/
 $__args_list = Array('Name','UserID','UniqID','IsDisabled','Prompt');
@@ -11,62 +10,65 @@ Eval(COMP_INIT);
 /******************************************************************************/
 /******************************************************************************/
 if(Is_Null($UserID))
-  $UserID = 1;
+	$UserID = 1;
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $User = DB_Select('Users',Array('ID','GroupID','Email','Name'),Array('UNIQ','ID'=>$UserID));
 #-------------------------------------------------------------------------------
 switch(ValueOf($User)){
-  case 'error':
-    return ERROR | @Trigger_Error(500);
-  case 'exception':
-    return ERROR | @Trigger_Error(400);
-  case 'array':
-    #---------------------------------------------------------------------------
-    $NoBody = new Tag('NOBODY');
-    #---------------------------------------------------------------------------
-    if(Is_Null($UniqID))
-      $UniqID = UniqID('ID');
-    #---------------------------------------------------------------------------
-    $Comp = Comp_Load(
-      'Form/Input',
-      Array(
-        'name'    => $UniqID,
-        'onfocus' => "value='';",
-        'onclick' => SPrintF("AutoComplite(this,GetPosition(this),'/Administrator/AutoComplite/UserID',function(Text,Value){form.%s.value = Text;form.%s.value = Value;});",$UniqID,$Name),
-        'type'    => 'text',
-        'value'   => SPrintF('%s (%s)',$User['Email'],$User['Name']),
-	'prompt'  => $Prompt?$Prompt:'Для поиска пользователя введите первые буквы его имени или email адреса',
-	'style'   => 'width:100%'
-      )
-    );
-    if(Is_Error($Comp))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    if($IsDisabled)
-      $Comp->AddAttribs(Array('disabled'=>TRUE));
-    #---------------------------------------------------------------------------
-    if($User['GroupID'] != 4000000)
-      $Comp->AddAttribs(Array('value'=>SPrintF('%s (%s)',$User['Email'],$User['Name'])));
-    #---------------------------------------------------------------------------
-    $NoBody->AddChild($Comp);
-    #---------------------------------------------------------------------------
-    $Comp = Comp_Load(
-      'Form/Input',
-      Array(
-        'name'  => $Name,
-        'type'  => 'hidden',
-        'value' => $User['ID']
-      )
-    );
-    if(Is_Error($Comp))
-      return ERROR | @Trigger_Error(500);
-    #---------------------------------------------------------------------------
-    $NoBody->AddChild($Comp);
-    #---------------------------------------------------------------------------
-    return $NoBody;
-  default:
-    return ERROR | @Trigger_Error(101);
+case 'error':
+	return ERROR | @Trigger_Error(500);
+case 'exception':
+	return ERROR | @Trigger_Error(400);
+case 'array':
+	#-------------------------------------------------------------------------------
+	$NoBody = new Tag('NOBODY');
+	#-------------------------------------------------------------------------------
+	if(Is_Null($UniqID))
+		$UniqID = UniqID('ID');
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load(
+			'Form/Input',
+			Array(
+				'name'		=> $UniqID,
+				'onfocus'	=> "value='';",
+				'onclick'	=> SPrintF("AutoComplite(this,GetPosition(this),'/Administrator/AutoComplite/UserID',function(Text,Value){form.%s.value = Text;form.%s.value = Value;});",$UniqID,$Name),
+				'type'		=> 'text',
+				'prompt'	=> $Prompt?$Prompt:'Для поиска пользователя введите первые буквы его имени или email адреса',
+				'style'		=> 'width:100%'
+				)
+			);
+	if(Is_Error($Comp))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	if($IsDisabled)
+		$Comp->AddAttribs(Array('disabled'=>TRUE));
+	#-------------------------------------------------------------------------------
+	if($User['GroupID'] != 4000000)
+		$Comp->AddAttribs(Array('value'=>SPrintF('%s (%s)',$User['Email'],$User['Name'])));
+	#-------------------------------------------------------------------------------
+	$NoBody->AddChild($Comp);
+	#-------------------------------------------------------------------------------
+	$Comp = Comp_Load(
+			'Form/Input',
+			Array(
+				'name'	=> $Name,
+				'type'	=> 'hidden',
+				'value'	=> $User['ID']
+			)
+		);
+	if(Is_Error($Comp))
+		return ERROR | @Trigger_Error(500);
+	#-------------------------------------------------------------------------------
+	$NoBody->AddChild($Comp);
+	#-------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------
+	return $NoBody;
+	#-------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------
+default:
+	return ERROR | @Trigger_Error(101);
 }
 #-------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------
 ?>
