a0e3d6a7db8164ffb933b57cf07472a10409d18b
Alex Keda <lissyara@HP.lissyara.su>
2022-05-30 21:57:58 +0300

    JBS-289: изменил сообщение о входе

--- hosts/billing/comp/www/API/Logon.comp.php
+++ hosts/billing/comp/www/API/Logon.comp.php
@@ -166,7 +166,7 @@ if(!SetCookie('Email',$User['Email'],Time() + 31536000,'/'))
 	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-$Event = Array('UserID'=>$User['ID'],'Text'=>SPrintF('Пользователь вошел в систему с IP-адреса (%s)',$_SERVER['REMOTE_ADDR']));
+$Event = Array('UserID'=>$User['ID'],'Text'=>SPrintF('Вход в систему с IP-адреса (%s)',$_SERVER['REMOTE_ADDR']));
 #-------------------------------------------------------------------------------
 $Event = Comp_Load('Events/EventInsert',$Event);
 if(!$Event)
