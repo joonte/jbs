8d6d5645d022542a4e9270593aec91eb3beb046d
Alex Keda <admin@lissyara.su>
2023-03-07 12:09:27 +0300

    JBS-1653: добавил предупреждение, что статьи видимы через АПИ

--- db/billing/db.sql
+++ db/billing/db.sql
@@ -3008,6 +3008,10 @@ INSERT INTO `Clauses` (`GroupID`, `AuthorID`, `EditorID`, `Partition`, `Title`, 
 INSERT INTO `Clauses` (`GroupID`, `AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `Text`, `IsPublish`) VALUES
 (6, 100, 100, 'Invoices/PaymentSystems/PayBox', 'Шаблон платежной системы PayBox', 'yes', 'yes', 'yes', '<NOBODY>\r\n <H1>\r\n СЧЕТ №%Invoice.Number% от %Invoice.CreateDate%\r\n</H1>\r\n <DIV id="Services">\r\n [список услуг]\r\n</DIV>\r\n <H2>\r\n Платежное поручение\r\n</H2>\r\n <TABLE border="1" cellpadding="5" cellspacing="0">\r\n  <TBODY>\r\n   <TR bgcolor="#DCDCDC">\r\n    <TD align="center">\r\n    Назначение\r\n   </TD>\r\n    <TD align="center">\r\n    Сумма\r\n   </TD>\r\n   </TR>\r\n   <TR>\r\n    <TD>\r\n    За web-услуги по счету №%Invoice.Number%\r\n   </TD>\r\n    <TD align="right">\r\n    %Invoice.Foreign% %PaymentSystem.Measure%\r\n   </TD>\r\n   </TR>\r\n  </TBODY>\r\n </TABLE>\r\n</NOBODY>\r\n', 'yes');
 -- SEPARATOR
+-- added by lissyara 2023-03-07 in 12:06 MSK
+INSERT INTO `Clauses` (`GroupID`, `AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `Text`, `IsPublish`) VALUES
+(6, 100, 100, 'Header:/Administrator/Clauses', 'Предупреждение по статьям', 'yes', 'yes', 'yes', '<NOBODY><DIV>Обратите внимание что опубликованные статьи доступны по АПИ, не надо в них хранить пароли, заметки и т.п.</DIV></NOBODY>', 'yes');
+-- SEPARATOR
 
 UNLOCK TABLES;
 -- SEPARATOR
---
+++ patches/billing/db/1000377.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `Clauses` (`GroupID`, `AuthorID`, `EditorID`, `Partition`, `Title`, `IsProtected`, `IsXML`, `IsDOM`, `Text`, `IsPublish`) VALUES
+(6, 100, 100, 'Header:/Administrator/Clauses', 'Предупреждение по статьям', 'yes', 'yes', 'yes', '<NOBODY><DIV>Обратите внимание что опубликованные статьи доступны по АПИ, не надо в них хранить пароли, заметки и т.п.</DIV></NOBODY>', 'yes');
+
