d39d2023c9d66883759a6e8a59ae6850291c1b62
Alex Keda <lissyara@HP.lissyara.su>
2023-06-28 21:32:46 +0300

    JBS-1695: вынес цвета статусов в конфиг

--- hosts/hosting/comp/Colors/DomainOrders.comp.php
+++ hosts/hosting/comp/Colors/DomainOrders.comp.php
@@ -1,67 +1,22 @@
 <?php
 
 #-------------------------------------------------------------------------------
-/** @author Великодный В.В. (Joonte Ltd.) */
+/** @author Alex Keda, for www.host-food.ru */
 /******************************************************************************/
 /******************************************************************************/
 $__args_list = Array('StatusID');
 /******************************************************************************/
 Eval(COMP_INIT);
 /******************************************************************************/
-switch($StatusID){
-  case 'Waiting':
-    $Color = 'F9E47D';
-  break;
-  case 'ClaimForRegister':
-    $Color = 'BCF0FF';
-  break;
-  case 'ForContractRegister':
-    $Color = 'ADC1F0';
-  break;
-  case 'OnContractRegister':
-    $Color = 'ADC1F0';
-  break;
-  case 'ForRegister':
-    $Color = 'ADC1F0';
-  break;
-  case 'OnRegister':
-    $Color = 'BCF0FF';
-  break;
-  case 'OnDelegating':
-    $Color = 'F1FCCE';
-  break;
-  case 'Active':
-    $Color = 'D5F66C';
-  break;
-  case 'ForProlong':
-    $Color = 'ADC1F0';
-  break;
-  case 'OnProlong':
-    $Color = 'ADC1F0';
-  break;
-  case 'ForNsChange':
-    $Color = 'ADC1F0';
-  break;
-  case 'OnNsChange':
-    $Color = 'ADC1F0';
-  break;
-  case 'Suspended':
-    $Color = 'FF6666';
-  break;
-  case 'Deleted':
-    $Color = 'DCDCDC';
-  break;
-  case 'ForTransfer':
-    $Color = 'ADD8E6';
-  break;
-  case 'OnTransfer':
-    $Color = 'E8FAFF';
-  break;
-  default:
-    $Color = '999999';
-}
+/******************************************************************************/
+$Config = Config();
+#-------------------------------------------------------------------------------
+$Statuses = $Config['Statuses']['DomainOrders'];
+#-------------------------------------------------------------------------------
+$Color = IsSet($Statuses[$StatusID]['Color'])?$Statuses[$StatusID]['Color']:999999;
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 return Array('bgcolor'=>SPrintF('#%s',$Color));
 #-------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------
 ?>
