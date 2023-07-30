836a498a112413444b37e255098895a07f77b5f7
Alex Keda <lissyara@HP.lissyara.su>
2023-07-30 15:08:39 +0300

    JBS-1702: сделал беспарольный переход в панель управления ISPmanager для наших DS и VPS

--- hosts/hosting/comp/www/API/OrderManage.comp.php
+++ hosts/hosting/comp/www/API/OrderManage.comp.php
@@ -41,7 +41,11 @@ if(Is_Error(System_Load(SPrintF('classes/%sServer.class.php',$Service['Code'])))
 	return ERROR | @Trigger_Error(500);
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-$Columns = Array('ID','UserID','StatusID','ServerID','StatusID');
+$Columns = Array(
+		'ID','UserID','StatusID','ServerID','StatusID','DependOrderID',
+		SPrintF('(SELECT `UserID` FROM `OrdersOwners` WHERE `OrdersOwners`.`ID` = `%sOrdersOwners`.`DependOrderID`) AS `DependOrderUserID`',$Service['Code']),
+		SPrintF('(SELECT `Code` FROM `Services` WHERE `Services`.`ID` = (SELECT `ServiceID` FROM `OrdersOwners` WHERE `OrdersOwners`.`ID` = `%sOrdersOwners`.`DependOrderID`)) AS `DependOrderCode`',$Service['Code'])
+		);
 #-------------------------------------------------------------------------------
 if(In_Array($Service['Code'], Array('ISPsw','DS'))){
 	#-------------------------------------------------------------------------------
@@ -74,8 +78,52 @@ $__USER = $GLOBALS['__USER'];
 if($Order['StatusID'] != 'Active' && !$__USER['IsAdmin'])
 	return new gException('ORDER_NOT_ACTIVE',SPrintF('Заказ услуги "%s" не активен',$Service['NameShort']));
 #-------------------------------------------------------------------------------
-if($Service['Code'] == 'ISPsw')
-	$Order = $Order + Array('Login'=>'root','Password'=>'my-mega-pass');
+if($Service['Code'] == 'ISPsw'){
+	#-------------------------------------------------------------------------------
+	// проверяем у юзера права на этот заказ, может не его вообще
+	$IsPermission = Permission_Check(SPrintF('%sManage',$Order['DependOrderCode']),(integer)$__USER['ID'],(integer)$Order['DependOrderUserID']);
+	#-------------------------------------------------------------------------------
+	switch(ValueOf($IsPermission)){
+	case 'error':
+		return ERROR | @Trigger_Error(500);
+	case 'exception':
+		return ERROR | @Trigger_Error(400);
+	case 'false':
+		return ERROR | @Trigger_Error(700);
+	case 'true':
+		break;
+	default:
+		return ERROR | @Trigger_Error(101);
+	}
+	#-------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------
+	// пробуем достать пароль, у заказа к которому относится лицензия
+	$DependOrder = DB_Select(SPrintF('%sOrdersOwners',$Order['DependOrderCode']),Array('Password'),Array('UNIQ','Where'=>SPrintF('`OrderID` = %u',$Order['DependOrderID'])));
+	#-------------------------------------------------------------------------------
+	switch(ValueOf($DependOrder)){
+	case 'error':
+		return ERROR | @Trigger_Error(500);
+	case 'exception':
+		#-------------------------------------------------------------------------------
+		$Password = 'my-mega-pass';
+		#-------------------------------------------------------------------------------
+		break;
+		#-------------------------------------------------------------------------------
+	case 'array':
+		#-------------------------------------------------------------------------------
+		$Password = $DependOrder['Password'];
+		#-------------------------------------------------------------------------------
+		break;
+		#-------------------------------------------------------------------------------
+	default:
+		return ERROR | @Trigger_Error(101);
+	}
+	#-------------------------------------------------------------------------------
+	$Order = $Order + Array('Login'=>'root','Password'=>$Password);
+	#-------------------------------------------------------------------------------
+	//Debug(print_r($Order,TRUE));
+	#-------------------------------------------------------------------------------
+}
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 $IsPermission = Permission_Check(SPrintF('%sManage',$Service['Code']),(integer)$__USER['ID'],(integer)$Order['UserID']);
