23bb25bc6ee994fb300ece1cc5cdc7946f909273
Alex Keda <lissyara@HP.lissyara.su>
2023-12-14 21:51:38 +0300

    JBS-1230: процессинг СберБанка теперь позваоляет оплату только своими картми или приложением/спасибом

---
+++ patches/billing/db/1000381.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `PaymentSystemsCollation` VALUES (NULL,'yes',17,'SberBank','SberPay','SberPay.png','Оплата через СберБанк: картами СберБанка, приложением, бонусами "Спасибо" и т.п.','SberPay/SberBank/Спасибо','');
+
