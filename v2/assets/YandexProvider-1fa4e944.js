287b4b7df95ef1a4a3f4a1784e9307aa1fecad0f
Alex Keda <admin@lissyara.su>
2024-04-24 10:38:16 +0300

    JBS-1653: пре-альфа нового интерфейса, 13

---
+++ v2/assets/YandexProvider-1fa4e944.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-58c913f0.js";const r={};function n(c,t){return" yandex "}const o=e(r,[["render",n]]);export{o as default};
--- v2/assets/YandexProvider-fac967d9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-7c70979b.js";const r={};function n(c,t){return" yandex "}const o=e(r,[["render",n]]);export{o as default};
