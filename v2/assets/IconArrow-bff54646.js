04fbcbd09feaf0c4a1b6744727def6e1006a1b5a
Alex Keda <lissyara@dacha.lissyara.su>
2024-05-09 19:21:17 +0300

    JBS-1653: пре-альфа нового интерфейса, 14

--- v2/assets/IconArrow-bff54646.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c as n,a as t}from"./index-58c913f0.js";const s={},r={fill:"none",height:"24",viewBox:"0 0 24 24",width:"24",xmlns:"http://www.w3.org/2000/svg"},c=t("path",{d:"m6 9 6 6 6-6",stroke:"currentColor","stroke-linecap":"round","stroke-linejoin":"round"},null,-1),_=[c];function i(a,l){return e(),n("svg",r,_)}const h=o(s,[["render",i]]);export{h as _};
