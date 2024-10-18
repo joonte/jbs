250a743640687f1dc6b75bfd2648cf8be9bc4f7f
Alex Keda <admin@lissyara.su>
2024-10-18 08:08:37 +0300

    JBS-1653: новый интерфейс, правки

---
+++ v2/assets/YandexProvider-4e269ba7.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-59dbe3d3.js";const r={};function n(c,t){return" yandex "}const o=e(r,[["render",n]]);export{o as default};
---
+++ v2/assets/promoStore-e0b1f805.js
@@ -0,0 +1,1 @@
+import{q as e}from"./index-59dbe3d3.js";const t=e("promo",{state:()=>({promoCode:""}),actions:{setPromoCode(o){this.promoCode=o}}});export{t as u};
