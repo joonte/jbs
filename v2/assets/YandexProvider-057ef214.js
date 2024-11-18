6268b5efd84d71ac315303343810b1b355e4542d
Alex Keda <admin@lissyara.su>
2024-11-18 08:35:23 +0300

    JBS-1653: новый интерфейс, АПИ для гостевых счетов

---
+++ v2/assets/YandexProvider-057ef214.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-3af25d4c.js";const r={};function n(c,t){return" yandex "}const o=e(r,[["render",n]]);export{o as default};
---
+++ v2/assets/promoStore-aaceb1af.js
@@ -0,0 +1,1 @@
+import{q as e}from"./index-3af25d4c.js";const t=e("promo",{state:()=>({promoCode:""}),actions:{setPromoCode(o){this.promoCode=o}}});export{t as u};
